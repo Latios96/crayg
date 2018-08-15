9add6e7e2b57988f7c9a5811e7ecad031c12b008
Jan <jan.honsbrok@gmail.com>
2018-08-15 18:39:38 +0200

    fiddling with bitmaps

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
+#include "bitmap_image.hpp"
 
 class Vector3f {
 public:
@@ -119,12 +120,104 @@ TEST_CASE("Vector3f crossProduct") {
 	REQUIRE(result.z == 3);
 }
 
+class Image {
+	int width, height;
+	float *values;
+
+	Image(int width, int height) {
+		this->width = width;
+		this->height = height;
+
+		int size = width * height * 3;
+		values = new float[size];
+	}
+
+	void setValue(int x, int y, const Vector3f &color) {
+		values[x + y] = color.x;
+		values[x + y+1] = color.y;
+		values[x + y+2] = color.z;
+	}
+
+	~Image() {
+		delete[] values;
+	}
+
+};
+
 int main(int argc, char* argv[]) {
 	// global setup...
 
-	int result = Catch::Session().run(argc, argv);
+	/*int result = Catch::Session().run(argc, argv);
 
 	// global clean-up...
 	system("PAUSE");
-	return result;
+	return result;*/
+
+	// https://stackoverflow.com/questions/2654480/writing-bmp-image-in-pure-c-c-without-other-libraries
+	const int w = 100; /* Put here what ever width you want */
+	const int h = 100; /* Put here what ever height you want */
+	int red[w][h];
+	int green[w][h];
+	int blue[w][h];
+
+
+	FILE *f;
+	unsigned char *img = NULL;
+	int filesize = 54 + 3 * w*h;  //w is your image width, h is image height, both int
+	if (img)
+		free(img);
+	img = (unsigned char *)malloc(3 * w*h);
+	memset(img, 0, sizeof(img));
+	int x;
+	int y;
+	int r;
+	int g;
+	int b;
+
+	for (int i = 0; i<w; i++)
+	{
+		for (int j = 0; j<h; j++)
+		{
+			x = i; y = (h - 1) - j;
+			r = red[i][j] * 255;
+			g = green[i][j] * 255;
+			b = blue[i][j] * 255;
+			if (r > 255) r = 255;
+			if (g > 255) g = 255;
+			if (b > 255) b = 255;
+			img[(x + y*w) * 3 + 2] = (unsigned char)(r);
+			img[(x + y*w) * 3 + 1] = (unsigned char)(g);
+			img[(x + y*w) * 3 + 0] = (unsigned char)(b);
+		}
+	}
+
+	unsigned char bmpfileheader[14] = { 'B','M', 0,0,0,0, 0,0, 0,0, 54,0,0,0 };
+	unsigned char bmpinfoheader[40] = { 40,0,0,0, 0,0,0,0, 0,0,0,0, 1,0, 24,0 };
+	unsigned char bmppad[3] = { 0,0,0 };
+
+	bmpfileheader[2] = (unsigned char)(filesize);
+	bmpfileheader[3] = (unsigned char)(filesize >> 8);
+	bmpfileheader[4] = (unsigned char)(filesize >> 16);
+	bmpfileheader[5] = (unsigned char)(filesize >> 24);
+
+	bmpinfoheader[4] = (unsigned char)(w);
+	bmpinfoheader[5] = (unsigned char)(w >> 8);
+	bmpinfoheader[6] = (unsigned char)(w >> 16);
+	bmpinfoheader[7] = (unsigned char)(w >> 24);
+	bmpinfoheader[8] = (unsigned char)(h);
+	bmpinfoheader[9] = (unsigned char)(h >> 8);
+	bmpinfoheader[10] = (unsigned char)(h >> 16);
+	bmpinfoheader[11] = (unsigned char)(h >> 24);
+
+	f = fopen("img.bmp", "wb");
+	fwrite(bmpfileheader, 1, 14, f);
+	fwrite(bmpinfoheader, 1, 40, f);
+	for (int i = 0; i<h; i++)
+	{
+		fwrite(img + (w*(h - i - 1) * 3), 3, w, f);
+		fwrite(bmppad, 1, (4 - (w * 3) % 4) % 4, f);
+	}
+	fclose(f);
+
+	return 0;
 }
