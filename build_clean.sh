7dafa62dff82a52d72454c4aa5260dfb5177871b
Jan <jan.honsbrok@gmail.com>
2018-11-13 21:09:59 +0100

    prepare code coverage measuring

--- build_clean.sh
+++ build_clean.sh
@@ -4,9 +4,12 @@ mkdir build
 cd build
 cmake ..
 make -j 4
-#lcov --directory ./build/ --capture --output-file ./code_coverage.info -rc lcov_branch_coverage=1
 make test
-#genhtml ./code_coverage.info --branch-coverage --output-directory ./code_coverage_report/
+
+#lcov --directory ./build/ --capture --output-file ./code_coverage.info -rc lcov_branch_coverage=1
+#lcov -e ./code_coverage.info $PWD/crayg/\* > ./code_coverage_clean.info
+#genhtml ./code_coverage_clean.info --branch-coverage --output-directory ./code_coverage_report/
 
 ./standalone/crayg-standalone -s ../example_scenes/single_sphere.json -o single_sphere.bmp
 ./standalone/crayg-standalone -s ../example_scenes/three_spheres.json -o three_spheres.bmp
+
