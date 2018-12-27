4356f2de582d7012001c743f0b73588569b1d33e
jan <jan.honsbrok@gmail.com>
2018-12-27 23:25:27 +0100

    added remainingTimeCalculator

---
+++ crayg/utils/RemainingTimeCalculator.h
@@ -0,0 +1,28 @@
+//
+// Created by jan on 27.12.18.
+//
+
+#ifndef CRAYG_REMAININGTIMECALCULATOR_H
+#define CRAYG_REMAININGTIMECALCULATOR_H
+
+
+#include <chrono>
+
+class RemainingTimeCalculator {
+
+public:
+    explicit RemainingTimeCalculator(std::chrono::steady_clock::time_point startTime);
+
+    /**
+     * Returns the remaining time for the given progress
+     * @param progress progress in percent, for example 5
+     * @return the remaining time in seconds
+     */
+    float getRemainingTimeByProgress(int progress);
+
+private:
+    std::chrono::steady_clock::time_point startTime;
+};
+
+
+#endif //CRAYG_REMAININGTIMECALCULATOR_H
---
+++ test/testRemainingTimeCalculator.cpp
@@ -0,0 +1,23 @@
+//
+// Created by jan on 27.12.18.
+//
+
+#include <catch.hpp>
+#include <utils/RemainingTimeCalculator.h>
+
+
+TEST_CASE("RemainingTimeCalculator") {
+    RemainingTimeCalculator remainingTimeCalculator(std::chrono::steady_clock::now());
+
+    SECTION("progressOfZeroShouldReturn-1") {
+        REQUIRE(remainingTimeCalculator.getRemainingTimeByProgress(0) == -1);
+    }
+
+    SECTION("progressOf100ShouldReturn0"){
+        REQUIRE(remainingTimeCalculator.getRemainingTimeByProgress(100) == 0);
+    }
+    SECTION("progressOf1ShouldReturnElapsedTime*100"){
+        RemainingTimeCalculator remainingTimeCalculator(std::chrono::steady_clock::now() - std::chrono::seconds(5));
+        REQUIRE(remainingTimeCalculator.getRemainingTimeByProgress(2) == 245);
+    }
+}
