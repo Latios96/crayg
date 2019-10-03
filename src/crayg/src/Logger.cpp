bb46b59a3aa03cad21737f3d0085fed38afb0cae
jan <jan.honsbrok@gmail.com>
2019-10-03 18:05:55 +0200

    add logger wrapper

---
+++ src/crayg/src/Logger.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Jan on 26.05.2019.
+//
+#include <Logger.h>
+void Logger::set_level(LevelEnum level) {
+    if (level == LevelEnum::trace) {
+        spdlog::set_level(spdlog::level::trace);
+    } else if (level == LevelEnum::debug) {
+        spdlog::set_level(spdlog::level::debug);
+    } else if (level == LevelEnum::info) {
+        spdlog::set_level(spdlog::level::info);
+    } else if (level == LevelEnum::warn) {
+        spdlog::set_level(spdlog::level::warn);
+    } else if (level == LevelEnum::err) {
+        spdlog::set_level(spdlog::level::err);
+    } else if (level == LevelEnum::critical) {
+        spdlog::set_level(spdlog::level::critical);
+    } else if (level == LevelEnum::off) {
+        spdlog::set_level(spdlog::level::off);
+    }
+}
---
+++ src/crayg/src/Logger.h
@@ -0,0 +1,112 @@
+//
+// Created by Jan on 21.05.2019.
+//
+
+#ifndef CRAYG_LOGGER_H_
+#define CRAYG_LOGGER_H_
+
+#include <spdlog/spdlog.h>
+
+class Logger {
+ public:
+
+    enum class LevelEnum {
+        trace = 0,
+        debug = 1,
+        info = 2,
+        warn = 3,
+        err = 4,
+        critical = 5,
+        off = 6
+    };
+    static void set_level(LevelEnum level);
+
+    template<typename... Args>
+    static void trace(const char *fmt, const Args &... args);
+
+    template<typename... Args>
+    static void debug(const char *fmt, const Args &... args);
+
+    template<typename... Args>
+    static void info(const char *fmt, const Args &... args);
+
+    template<typename... Args>
+    static void warning(const char *fmt, const Args &... args);
+
+    template<typename... Args>
+    static void error(const char *fmt, const Args &... args);
+
+    template<typename... Args>
+    static void critical(const char *fmt, const Args &... args);
+
+    template<typename T>
+    static void trace(const T &msg);
+
+    template<typename T>
+    static void debug(const T &msg);
+
+    template<typename T>
+    static void info(const T &msg);
+
+    template<typename T>
+    static void warning(const T &msg);
+
+    template<typename T>
+    static void critical(const T &msg);
+};
+
+template<typename... Args>
+void Logger::trace(const char *fmt, const Args &... args) {
+    spdlog::trace(fmt, args...);
+}
+
+template<typename... Args>
+void Logger::debug(const char *fmt, const Args &... args) {
+    spdlog::debug(fmt, args...);
+}
+
+template<typename... Args>
+void Logger::info(const char *fmt, const Args &... args) {
+    spdlog::info(fmt, args...);
+}
+
+template<typename... Args>
+void Logger::warning(const char *fmt, const Args &... args) {
+    spdlog::warn(fmt, args...);
+}
+
+template<typename... Args>
+void Logger::error(const char *fmt, const Args &... args) {
+    spdlog::error(fmt, args...);
+}
+
+template<typename... Args>
+void Logger::critical(const char *fmt, const Args &... args) {
+    spdlog::critical(fmt, args...);
+}
+template<typename T>
+void Logger::trace(const T &msg) {
+    spdlog::trace(msg);
+}
+
+template<typename T>
+void Logger::debug(const T &msg) {
+    spdlog::debug(msg);
+}
+
+template<typename T>
+void Logger::info(const T &msg) {
+    spdlog::info(msg);
+}
+
+template<typename T>
+void Logger::warning(const T &msg) {
+    spdlog::warn(msg);
+}
+
+template<typename T>
+void Logger::critical(const T &msg) {
+    spdlog::critical(msg);
+}
+
+#endif //RASPITOLIGHT_SRC_CORE_LIB_LOGGER_H_
