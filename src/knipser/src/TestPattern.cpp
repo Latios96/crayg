c78269275700e28d79a2dee17c7de19b01492fd1
Jan <jan.honsbrok@gmail.com>
2020-01-10 05:00:37 +0100

    fix tests

--- src/knipser/src/TestPattern.cpp
+++ src/knipser/src/TestPattern.cpp
@@ -12,6 +12,7 @@ TestPattern::TestPattern(std::string pattern) : pattern(std::move(pattern)) {}
 bool TestPattern::matches(const std::string &testName) const {
     std::regex re(pattern, std::regex_constants::icase);
     std::smatch match;
+    std::regex_match(testName, match, re);
     return match[0].matched && match[0].length() == testName.size();
 }
 bool TestPattern::operator==(const TestPattern &rhs) const {
@@ -21,6 +22,6 @@ bool TestPattern::operator!=(const TestPattern &rhs) const {
     return !(rhs == *this);
 }
 std::ostream &operator<<(std::ostream &os, const TestPattern &pattern) {
-    os << "pattern: \"" << pattern.pattern << "\"" ;
+    os << "pattern: \"" << pattern.pattern << "\"";
     return os;
 }
