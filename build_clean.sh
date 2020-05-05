a1986468b53ec72154fdebb654c323b2d9190891
Jan Honsbrok <jan.honsbrok@gmail.com>
2020-05-05 18:19:54 +0200

    fix build_clean.sh

--- build_clean.sh
+++ build_clean.sh
@@ -2,13 +2,7 @@
 rm -rf build/
 mkdir build
 cd build
-conan install .. --build missing
-cmake ..
-make -j 4
-./bin/tests
-./bin/crayg-standalone-tests
-
-./bin/crayg-standalone -s ../example_scenes/single_sphere.json -o single_sphere.bmp
-./bin/crayg-standalone -s ../example_scenes/three_spheres.json -o three_spheres.bmp
-./bin/crayg-standalone -s ../example_scenes/poly_cube.json -o poly_cube.bmp
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j
+ctest -V
 
