c19878a3ecc33be374b06064a9e4f5c16b77fe70
Jan <jan.honsbrok@gmail.com>
2020-10-21 17:22:43 +0200

    add Bucket class

---
+++ src/crayg/src/image/Bucket.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Jan on 21.10.2020.
+//
+
+#include "Bucket.h"
+Bucket::Bucket(int x, int y, int width, int height) : x(x), y(y), width(width), height(height) {}
---
+++ src/crayg/src/image/Bucket.h
@@ -0,0 +1,14 @@
+//
+// Created by Jan on 21.10.2020.
+//
+
+#ifndef CRAYG_SRC_CRAYG_SRC_IMAGE_BUCKET_H_
+#define CRAYG_SRC_CRAYG_SRC_IMAGE_BUCKET_H_
+
+class Bucket {
+ public:
+    const int x, y, width, height;
+    Bucket(int x, int y, int width, int height);
+};
+
+#endif //CRAYG_SRC_CRAYG_SRC_IMAGE_BUCKET_H_
