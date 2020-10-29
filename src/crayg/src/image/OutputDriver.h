42f53da9dab1b99978da5652ddb7033234a458c5
Jan <jan.honsbrok@gmail.com>
2020-10-29 21:25:38 +0100

    fix virtual destructor

--- src/crayg/src/image/OutputDriver.h
+++ src/crayg/src/image/OutputDriver.h
@@ -11,7 +11,7 @@ class OutputDriver {
  public:
     virtual void prepareBucket(const ImageBucket &imageBucket) = 0;
     virtual void writeBucketImageBuffer(const BucketImageBuffer &bucketImageBuffer) = 0;
-    virtual ~OutputDriver() = 0;
+    virtual ~OutputDriver() = default;
 };
 
 #endif //CRAYG_SRC_CRAYG_SRC_IMAGE_OUTPUTDRIVER_H_
