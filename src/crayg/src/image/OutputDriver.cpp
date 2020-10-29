52fbc71e64699c0e73309e361d8e7367539f50f9
Jan <jan.honsbrok@gmail.com>
2020-10-29 20:50:09 +0100

    add OutputDriver class

---
+++ src/crayg/src/image/OutputDriver.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jan on 29.10.2020.
+//
+
+#include "OutputDriver.h"
---
+++ src/crayg/src/image/OutputDriver.h
@@ -0,0 +1,17 @@
+//
+// Created by Jan on 29.10.2020.
+//
+
+#ifndef CRAYG_SRC_CRAYG_SRC_IMAGE_OUTPUTDRIVER_H_
+#define CRAYG_SRC_CRAYG_SRC_IMAGE_OUTPUTDRIVER_H_
+
+#include "BucketImageBuffer.h"
+
+class OutputDriver {
+ public:
+    virtual void prepareBucket(const ImageBucket &imageBucket) = 0;
+    virtual void writeBucketImageBuffer(const BucketImageBuffer &bucketImageBuffer) = 0;
+    virtual ~OutputDriver() = 0;
+};
+
+#endif //CRAYG_SRC_CRAYG_SRC_IMAGE_OUTPUTDRIVER_H_
