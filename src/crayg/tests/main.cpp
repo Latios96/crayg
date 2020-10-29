6ce7cc8eb79dc6d35005e3182a96cc4aa4542ab0
Jan <jan.honsbrok@gmail.com>
2020-10-29 09:31:45 +0100

    remove stack trace

--- src/crayg/tests/main.cpp
+++ src/crayg/tests/main.cpp
@@ -1,11 +1,8 @@
 #define CATCH_CONFIG_RUNNER
 
 #include <catch2/catch.hpp>
-#include "stacktrace_registration.h"
 
 int main(int argc, char *argv[]) {
-    register_signal_handlers();
-    std::cout << signal_handler_registration_report() << std::endl;
     int result = Catch::Session().run(argc, argv);
     return result;
 }
