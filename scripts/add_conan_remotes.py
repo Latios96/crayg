398a40d36261bc1929dcb335fdf4a15f4c0f9662
Jan <jan.honsbrok@gmail.com>
2020-11-03 16:29:51 +0100

    faster builds for Github Actions

---
+++ scripts/add_conan_remotes.py
@@ -0,0 +1,43 @@
+import json
+import os
+import pprint
+
+
+def ensure_remote(remotes, name, url):
+    filtered_remotes = list(filter(lambda remote: remote["name"] == name, remotes))
+    remote_exists = bool(filtered_remotes)
+
+    if remote_exists:
+        remote = filtered_remotes[0]
+        remotes.remove(remote)
+    remotes.insert(0, {"name": name, "url": url, "verify_ssl": True})
+
+
+def main():
+    config_path = os.path.expanduser("~/.conan/remotes.json")
+    with open(config_path) as f:
+        remotes = json.load(f)["remotes"]
+
+    ensure_remote(
+        remotes, "bincrafters", "https://api.bintray.com/conan/bincrafters/public-conan"
+    )
+    ensure_remote(
+        remotes, "ppodsiadly", "https://api.bintray.com/conan/ppodsiadly/conan"
+    )
+    ensure_remote(
+        remotes, "my_bintray", "https://api.bintray.com/conan/latios96/my_conan"
+    )
+    ensure_remote(
+        remotes,
+        "framebffr",
+        "http://185.245.96.20:8081/artifactory/api/conan/conantest",
+    )
+
+    print(pprint.pformat(remotes))
+
+    with open(config_path, "w") as f:
+        json.dump({"remotes": remotes}, f, indent=4)
+
+
+if __name__ == "__main__":
+    main()
