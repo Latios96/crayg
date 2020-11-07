c751871f0b184c36719bec17fe7a2f73ab5922b5
Jan <jan.honsbrok@gmail.com>
2020-11-07 12:17:18 +0100

    fix build_vs_2017.bat

--- build_vs_2017.bat
+++ build_vs_2017.bat
@@ -3,4 +3,4 @@ cd build
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 C:\cmake\bin\cmake -G "Visual Studio 15 2017 Win64" .. -DCMAKE_BUILD_TYPE=Release
 C:\cmake\bin\cmake --build . --config Release
-ctest -V
+ctest -C Release -V
