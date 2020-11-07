2c4fbcd2dd8423ed97e1a5df9de2ab634f0a6d80
Jan <jan.honsbrok@gmail.com>
2020-11-07 13:06:23 +0100

    bump version

--- src/crayg/src/CraygInfo.cpp
+++ src/crayg/src/CraygInfo.cpp
@@ -3,5 +3,5 @@
 //
 
 #include "CraygInfo.h"
-const std::string CraygInfo::VERSION = "1.4.0";
+const std::string CraygInfo::VERSION = "1.5.0";
 const std::string CraygInfo::COMMIT_HASH = std::string(GIT_COMMIT_HASH);
