8c5042a1f0efaba225f2f7c7331ab66076c95c16
Jan <jan.honsbrok@gmail.com>
2020-11-09 20:22:04 +0100

    style

--- src/crayg/src/basics/Transform.h
+++ src/crayg/src/basics/Transform.h
@@ -6,13 +6,13 @@
 #define CRAYG_SRC_CRAYG_SRC_BASICS_TRANSFORM_H_
 
 #include "Matrix4x4f.h"
+
 class Transform {
  public:
     Transform();
     explicit Transform(const Matrix4x4f &matrix);
     static Transform fromPosition(const Vector3f &vector3f);
     // normal
-    // transform xyz
     // transform rotate xyz
     // scale xyz
     Vector3f apply(const Vector3f &vector3f);
