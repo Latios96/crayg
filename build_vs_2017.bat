e555c1564ba2161aeb588971bfcc4d3b50bb6f85
Jan <jan.honsbrok@gmail.com>
2020-11-12 14:26:26 +0100

    use multithreaded builds on windows

--- build_vs_2017.bat
+++ build_vs_2017.bat
@@ -2,6 +2,6 @@ mkdir build
 cd build
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 C:\cmake\bin\cmake -G "Visual Studio 15 2017 Win64" .. -DCMAKE_BUILD_TYPE=Release
-C:\cmake\bin\cmake --build . --config Release
+C:\cmake\bin\cmake --build . --config Release -- /M:%NUMBER_OF_PROCESSORS%
 ctest -C Release -V
 C:\cmake\bin\cmake --build . --config Release --target createDistribution
