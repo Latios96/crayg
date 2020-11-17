1b31951226df864e601232db0c9b04f14ac4f35e
Jan <jan.honsbrok@gmail.com>
2020-11-17 16:48:54 +0100

    add test case for light sampling

--- src/crayg/tests/TestLight.cpp
+++ src/crayg/tests/TestLight.cpp
@@ -4,7 +4,9 @@
 #include <catch2/catch.hpp>
 #include <fakeit.hpp>
 #include <sceneIO/Serializable.h>
+#include <scene/Scene.h>
 #include "scene/Light.h"
+#include "intersectors/SceneIntersector.h"
 
 TEST_CASE("Light") {
     Light myLight;
@@ -35,5 +37,52 @@ TEST_CASE("Light") {
 
         REQUIRE(myLight.getPosition() == Vector3f(1, 2, 3));
         REQUIRE(myLight.getIntensity() == 3.0f);
+    }
+}
+
+// for some reason fakeit did not work here
+class MockSceneIntersector : public SceneIntersector {
+ public:
+    MockSceneIntersector(Scene &scene, const Imageable::Intersection &return_value)
+        : SceneIntersector(scene) {
+        this->return_value = return_value;
+    }
+    Imageable::Intersection intersect(const Ray &ray) const override {
+        return return_value;
+    };
+ private:
+    Imageable::Intersection return_value;
+};
+
+TEST_CASE("Light Sampling") {
+    const float NO_SHADOW = 1.0f;
+    const float FULL_SHADOW = 0.0f;
+
+    Light light;
+    light.setPosition({0, 5, 0});
+    light.setIntensity(1.0);
+
+    SECTION("noIntersectionShouldReturnNoShadow") {
+        Scene scene;
+        MockSceneIntersector mockIntersector(scene, Imageable::Intersection::createInvalid());
+
+        const float shadowFactor = light.calculateShadowFactor(mockIntersector, {0, 0, 0});
+        REQUIRE(shadowFactor == NO_SHADOW);
+    }
+
+    SECTION("intersectionIsBehindLight") {
+        Scene scene;
+        MockSceneIntersector mockIntersector(scene, {10, std::make_shared<Sphere>()});
+
+        const float shadowFactor = light.calculateShadowFactor(mockIntersector, {0, 0, 0});
+        REQUIRE(shadowFactor == NO_SHADOW);
+    }
+
+    SECTION("intersectionIsBeforeLight") {
+        Scene scene;
+        MockSceneIntersector mockIntersector(scene, {2, std::make_shared<Sphere>()});
+
+        const float shadowFactor = light.calculateShadowFactor(mockIntersector, {0, 0, 0});
+        REQUIRE(shadowFactor == FULL_SHADOW);
     }
 }
