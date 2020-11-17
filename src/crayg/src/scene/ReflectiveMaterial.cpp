7e586f5e74b0b69d7d31f0fb9a26ceb60902ee5e
Jan <jan.honsbrok@gmail.com>
2020-11-17 11:45:43 +0100

    add ReflectiveMaterial

---
+++ src/crayg/src/scene/ReflectiveMaterial.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Jan on 17.11.2020.
+//
+
+#include "ReflectiveMaterial.h"
+
+Color ReflectiveMaterial::getDiffuseColor() {
+    return {};
+}
+float ReflectiveMaterial::reflectivity() {
+    return 1;
+}
+void ReflectiveMaterial::connectOutputToInput(const std::string &inputPlugName, PlugPtr outputPlug) {
+
+}
+PlugPtr ReflectiveMaterial::getPlugByName(const std::string &inputPlugName) {
+    return {};
+}
+void ReflectiveMaterial::serialize(Serializer &serializer) {
+    ShadingNode::serialize(serializer);
+    serializer.writeType("ReflectiveMaterial");
+}
+void ReflectiveMaterial::deserialize(Deserializer &deserializer) {
+    ShadingNode::deserialize(deserializer);
+}
---
+++ src/crayg/src/scene/ReflectiveMaterial.h
@@ -0,0 +1,20 @@
+//
+// Created by Jan on 17.11.2020.
+//
+
+#ifndef CRAYG_SRC_CRAYG_SRC_SCENE_REFLECTIVEMATERIAL_H_
+#define CRAYG_SRC_CRAYG_SRC_SCENE_REFLECTIVEMATERIAL_H_
+
+#include "Material.h"
+
+class ReflectiveMaterial : public Material {
+ public:
+    Color getDiffuseColor() override;
+    float reflectivity() override;
+    void connectOutputToInput(const std::string &inputPlugName, PlugPtr outputPlug) override;
+    PlugPtr getPlugByName(const std::string &inputPlugName) override;
+    void serialize(Serializer &serializer) override;
+    void deserialize(Deserializer &deserializer) override;
+};
+
+#endif //CRAYG_SRC_CRAYG_SRC_SCENE_REFLECTIVEMATERIAL_H_
