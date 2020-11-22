37eb75f6842d77a402da5da1d072ad65da3e3fe4
Jan <jan.honsbrok@gmail.com>
2020-11-22 13:56:11 +0100

    use algorithm

--- src/crayg/src/intersectors/SceneIntersector.cpp
+++ src/crayg/src/intersectors/SceneIntersector.cpp
@@ -2,6 +2,7 @@
 // Created by Jan Honsbrok on 01.11.18.
 //
 
+#include <algorithm>
 #include "SceneIntersector.h"
 Imageable::Intersection SceneIntersector::intersect(const Ray &ray) const {
     Imageable::Intersection hitIntersection(std::numeric_limits<float>::max(), nullptr);
@@ -14,29 +15,13 @@ Imageable::Intersection SceneIntersector::intersect(const Ray &ray) const {
             hitIntersection.imageable = intersection.imageable;
         }
     }
-    /*for (const auto &intersectable : scene.lights) {
-        Imageable::Intersection intersection = intersectable->intersect(ray);
-
-        if (intersection.rayParameter < hitIntersection.rayParameter && intersection.imageable) {
-            hitIntersection.rayParameter = intersection.rayParameter;
-            hitIntersection.imageable = intersection.imageable;
-        }
-    }*/
     return hitIntersection;
 }
 
 bool SceneIntersector::isIntersecting(const Ray &ray) const {
-    for (const auto &intersectable : scene.objects) {
-        if (intersectable->isIntersecting(ray)) {
-            return true;
-        }
-    }
-    /*for (const auto &intersectable : scene.lights) {
-        if (intersectable->isIntersecting(ray)) {
-            return true;
-        }
-    }*/
-    return false;
+    return std::any_of(scene.objects.begin(),
+                       scene.objects.end(),
+                       [&ray](std::shared_ptr<SceneObject> &intersectable) { return intersectable->isIntersecting(ray); });
 }
 
 SceneIntersector::SceneIntersector(Scene &scene) : scene(scene) {}
