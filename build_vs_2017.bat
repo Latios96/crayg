b0f3826747716f698af3ce82bb885104dc0be66c
Jan <jan.honsbrok@gmail.com>
2020-11-22 13:21:32 +0100

    use ninja build system

--- build_vs_2017.bat
+++ build_vs_2017.bat
@@ -1,7 +1,7 @@
 mkdir build
 cd build
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-C:\cmake\bin\cmake -G "Visual Studio 15 2017 Win64" .. -DCMAKE_BUILD_TYPE=Release
-C:\cmake\bin\cmake --build . --config Release -- /M:%NUMBER_OF_PROCESSORS%
+C:\cmake\bin\cmake -G "Ninja" .. -DCMAKE_BUILD_TYPE=Release
+C:\cmake\bin\cmake --build . --config Release
 ctest -C Release -V
 C:\cmake\bin\cmake --build . --config Release --target createDistribution
