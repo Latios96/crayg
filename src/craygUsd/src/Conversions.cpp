806a47a6f640baf8b77392655a8ba97e5f36b949
Jan Honsbrok <jan.honsbrok@gmail.com>
2021-10-28 19:43:32 +0200

    implement basic conversion for pxr::GfMatrix4d

--- src/craygUsd/src/Conversions.cpp
+++ src/craygUsd/src/Conversions.cpp
@@ -5,22 +5,24 @@
 #include "Conversions.h"
 namespace crayg {
 
+const Matrix4x4f MIRROR_Z = Matrix4x4f::scale(1, 1, -1);
+
 Matrix4x4f Conversions::convert(const pxr::GfMatrix4d &matrix) {
-    return {static_cast<float>(matrix[0][0]),
-            static_cast<float>(matrix[0][1]),
-            static_cast<float>(matrix[0][2]),
-            static_cast<float>(matrix[0][3]),
-            static_cast<float>(matrix[1][0]),
-            static_cast<float>(matrix[1][1]),
-            static_cast<float>(matrix[1][2]),
-            static_cast<float>(matrix[1][3]),
-            static_cast<float>(matrix[2][0]),
-            static_cast<float>(matrix[2][1]),
-            static_cast<float>(matrix[2][2]),
-            static_cast<float>(matrix[2][3]),
-            static_cast<float>(matrix[3][0]),
-            static_cast<float>(matrix[3][1]),
-            static_cast<float>(matrix[3][2]),
-            static_cast<float>(matrix[3][3])};
+    return Matrix4x4f(static_cast<float>(matrix[0][0]),
+                      static_cast<float>(matrix[1][0]),
+                      static_cast<float>(matrix[2][0]),
+                      static_cast<float>(matrix[3][0]),
+                      static_cast<float>(matrix[0][1]),
+                      static_cast<float>(matrix[1][1]),
+                      static_cast<float>(matrix[2][1]),
+                      static_cast<float>(matrix[3][1]),
+                      static_cast<float>(matrix[0][2]),
+                      static_cast<float>(matrix[1][2]),
+                      static_cast<float>(matrix[2][2]),
+                      static_cast<float>(matrix[3][2]),
+                      static_cast<float>(matrix[0][3]),
+                      static_cast<float>(matrix[1][3]),
+                      static_cast<float>(matrix[2][3]),
+                      static_cast<float>(matrix[3][3])) * MIRROR_Z;
 }
 };
--- src/craygUsd/tests/TestConversions.cpp
+++ src/craygUsd/tests/TestConversions.cpp
@@ -11,13 +11,31 @@ namespace crayg {
 
 TEST_CASE("ConvertMatrix4d") {
 
-    SECTION("should convert identy matrix") {
-        pxr::GfMatrix4d matrix;
+    SECTION("should convert identity matrix") {
+        pxr::GfMatrix4d matrix {};
         matrix.SetIdentity();
+        Matrix4x4f expectedMatrix(1, 0, 0, 0,
+                                  0, 1, 0, 0,
+                                  0, 0, -1, 0,
+                                  0, 0, 0, 1);
 
         const Matrix4x4f convertedMatrix = Conversions::convert(matrix);
 
-        REQUIRE(convertedMatrix == Matrix4x4f());
+        REQUIRE(convertedMatrix == expectedMatrix);
+    }
+
+    SECTION("should convert translated matrix") {
+        pxr::GfMatrix4d matrix {};
+        matrix.SetIdentity();
+        matrix.SetTranslate({1, 2, 3});
+        Matrix4x4f expectedMatrix(1, 0, 0, 1,
+                                  0, 1, 0, 2,
+                                  0, 0, -1, 3,
+                                  0, 0, 0, 1);
+
+        const Matrix4x4f convertedMatrix = Conversions::convert(matrix);
+
+        REQUIRE(convertedMatrix.isEqualTo(expectedMatrix, 0.000f));
     }
 }
 
