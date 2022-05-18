279ad6962b549b3ea4f6c05f81ae045a9df0464b
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-05-18 14:45:44 +0200

    wip: parsing and rendering USD scenes

--- src/usdexplorations/src/main.cpp
+++ src/usdexplorations/src/main.cpp
@@ -4,6 +4,7 @@
 #include <pxr/usd/usd/prim.h>
 #include <pxr/usd/usd/primRange.h>
 #include <pxr/usd/usdGeom/mesh.h>
+#include <pxr/usd/usdGeom/camera.h>
 #include <pxr/imaging/hd/meshUtil.h>
 #include <pxr/imaging/hd/meshTopology.h>
 #include <pxr/base/vt/array.h>
@@ -15,69 +16,65 @@
 #include "Renderer.h"
 #include "utils/ImagePathResolver.h"
 #include "scene/DiffuseMaterial.h"
+#include "UsdMeshTranslator.h"
+#include "UsdSphereLightTranslator.h"
+#include "UsdCameraTranslator.h"
 #include <image/ImageWriters.h>
 
 int main(int argc, char *argv[]) {
     crayg::StopWatch stopWatchOpeningState = crayg::StopWatch::createStopWatch("Opening stage");
-    auto stage = pxr::UsdStage::Open("M:\\modelLibrary\\_input_to_convert\\Kitchen_set\\Kitchen_set\\Kitchen_set.usd");
+    //const std::string usdFilePath = "M:\\modelLibrary\\_input_to_convert\\Kitchen_set\\Kitchen_set\\Kitchen_set.usd";
+    //const std::string usdFilePath = "M:\\test\\kitchenExportTest.usd";
+    //const std::string usdFilePath = "M:\\test\\experimentScene.usd";
+    const std::string usdFilePath = "C:\\workspace\\crayg\\debug_scenes\\kitchen_with_camera.usd";
+    //const std::string usdFilePath = "M:\\test\\test.usda";
+    //const std::string usdFilePath = "M:\\modelLibrary\\_input_to_convert\\USD_ALab_0730\\USD_ALab_0730\\entry.usda";
     //auto stage = pxr::UsdStage::Open("C:\\workspace\\crayg\\example_scenes\\polyPlane.usda");
-    stopWatchOpeningState.end();
+    auto stage = pxr::UsdStage::Open(usdFilePath);
 
     crayg::StopWatch stopWatchConvertMeshes = crayg::StopWatch::createStopWatch("Converting meshes");
-    crayg::RenderSettings renderSettings(crayg::Resolution(1280, 720), 1);
+    crayg::RenderSettings renderSettings(crayg::Resolution(1280, 720), 4);
     crayg::Scene scene;
     scene.renderSettings = renderSettings;
-    scene.camera = std::make_shared<crayg::Camera>(crayg::Vector3f(362, -27, 107),
-                                                   crayg::Vector3f(0, 1, 0),
-                                                   crayg::Vector3f(-1, 0, 0),
-                                                   35.0f,
-                                                   36.0f);
 
     auto defaultMaterial = std::make_shared<crayg::DiffuseMaterial>("defaultMaterial", crayg::Color::createWhite());
 
+    const int cameraIndexToRender = 0;
+    int cameraIndex = 0;
+
+    crayg::Logger::setLevel(crayg::Logger::LevelEnum::debug);
+
     for (pxr::UsdPrim prim: stage->TraverseAll()) {
         if (prim.IsA<pxr::UsdGeomMesh>()) {
-            pxr::UsdGeomMesh mesh(prim);
-            pxr::TfToken scheme;
-            mesh.GetSubdivisionSchemeAttr().Get(&scheme);
-            pxr::TfToken orientation;
-            mesh.GetOrientationAttr().Get(&orientation);
-            pxr::VtIntArray faceVertexCounts, faceVertexIndices, holeIndices;
-            mesh.GetFaceVertexCountsAttr().Get(&faceVertexCounts);
-            mesh.GetFaceVertexIndicesAttr().Get(&faceVertexIndices);
-            mesh.GetHoleIndicesAttr().Get(&holeIndices);
-            pxr::HdMeshTopology topology(scheme, orientation, faceVertexCounts, faceVertexIndices, holeIndices);
-            pxr::HdMeshUtil meshUtil(&topology, mesh.GetPath());
-            pxr::VtVec3iArray triangleIndices;
-            pxr::VtIntArray primitiveParams;
-            meshUtil.ComputeTriangleIndices(&triangleIndices, &primitiveParams);
-
-            auto triangleMesh = std::make_shared<crayg::TriangleMesh>();
-            pxr::VtVec3fArray points;
-            mesh.GetPointsAttr().Get(&points);
-            triangleMesh->points.reserve(points.size());
-            for (const auto &point: points) {
-                triangleMesh->points.emplace_back(point[0], point[1], point[2]);
-            }
-            triangleMesh->faceIndexes.reserve(triangleIndices.size());
-            for (const auto &faceIndex: triangleIndices) {
-                triangleMesh->faceIndexes.push_back(faceIndex[0]);
-                triangleMesh->faceIndexes.push_back(faceIndex[1]);
-                triangleMesh->faceIndexes.push_back(faceIndex[2]);
-            }
+            crayg::Logger::debug("Translating mesh {}", prim.GetPath().GetString());
+            auto triangleMesh = crayg::UsdMeshTranslator(pxr::UsdGeomMesh(prim)).translate();
             triangleMesh->init();
-            /*pxr::GfMatrix4d matrix;
-            bool resetsXformStack = false;
-            mesh.GetLocalTransformation(&matrix, &resetsXformStack);
-            std::cout << matrix << std::endl;*/
+            auto randomColor = std::make_shared<crayg::DiffuseMaterial>("defaultMaterial",
+                                                                        crayg::Color::createRandom();
             triangleMesh->setMaterial(defaultMaterial);
             scene.addObject(triangleMesh);
+        } else if (prim.IsA<pxr::UsdLuxSphereLight>()) {
+            crayg::Logger::debug("Translating light {}", prim.GetPath().GetString());
+            auto light = crayg::UsdSphereLightTranslator(pxr::UsdLuxSphereLight(prim)).translate();
+            scene.addLight(light);
+        } else if (prim.IsA<pxr::UsdGeomCamera>()) {
+            if (cameraIndex == cameraIndexToRender) {
+                crayg::Logger::debug("Using camera {}", prim.GetPath().GetString());
+                auto camera = crayg::UsdCameraTranslator(pxr::UsdGeomCamera(prim)).translate();
+                crayg::Logger::debug("{}", camera->getPosition());
+                scene.camera = camera;
+            }
+            cameraIndex++;
         }
     }
+
     stopWatchConvertMeshes.end();
 
-    auto light = std::make_shared<crayg::Light>(crayg::Transform::withPosition({0, 0, 130}), 1);
-    scene.addLight(light);
+    if (scene.camera == nullptr) {
+        crayg::Logger::error("No camera found in USD file {}!", *scene.camera);
+        crayg::Logger::error("Abort.");
+        exit(1);
+    }
 
     crayg::Image myImage(scene.renderSettings.resolution);
 
@@ -87,7 +84,7 @@ int main(int argc, char *argv[]) {
     renderer.renderScene();
 
     crayg::ImagePathResolver imagePathResolver;
-    std::string imageOutputPath = imagePathResolver.resolve("usdexplore.png");
+    std::string imageOutputPath = imagePathResolver.resolve("usdexplore.#.png");
     crayg::Logger::info("writing image to {}..", imageOutputPath);
     crayg::ImageWriters::writeImage(myImage, imageOutputPath);
     crayg::Logger::info("writing image done.");
