88975f899f182fd0160fa86b236840f5da0ccf1c
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-06-02 18:52:59 +0200

    tests: tests for UsdPathFactory

---
+++ src/crayg/tests/TestUsdPathFactory.cpp
@@ -0,0 +1,39 @@
+//
+// Created by Jan on 02.06.2022.
+//
+#include <catch2/catch.hpp>
+#include "sceneIO/write/usd/UsdPathFactory.h"
+
+namespace crayg {
+
+TEST_CASE("UsdPathFactory::getPathForName") {
+
+    UsdPathFactory usdPathFactory;
+
+    SECTION("should return name if name is not empty and already starts with '/'") {
+        const pxr::SdfPath path = usdPathFactory.getPathForName("/sphere", "Sphere");
+
+        REQUIRE(path.GetString() == "/sphere");
+    }
+
+    SECTION("should prepend '/' to name if name is not empty") {
+        const pxr::SdfPath path = usdPathFactory.getPathForName("sphere", "Sphere");
+
+        REQUIRE(path.GetString() == "/sphere");
+    }
+
+    SECTION("should return type and counter for empty name") {
+        const pxr::SdfPath path = usdPathFactory.getPathForName("", "Sphere");
+
+        REQUIRE(path.GetString() == "/Sphere0");
+    }
+
+    SECTION("should return incremented counter") {
+        usdPathFactory.getPathForName("", "Sphere");
+        const pxr::SdfPath path = usdPathFactory.getPathForName("", "Sphere");
+
+        REQUIRE(path.GetString() == "/Sphere1");
+    }
+}
+
+}
