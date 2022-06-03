5d9cd8cc628f2d2f3011ea7fe98ec07a2ba4d4d7
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-06-03 12:42:12 +0200

    feat: outline scene writing with usd

--- src/crayg/tests/TestUsdRenderSettingsWriter.cpp
+++ src/crayg/tests/TestUsdRenderSettingsWriter.cpp
@@ -1,5 +1,4 @@
 #include <catch2/catch.hpp>
-#include "sceneIO/write/usd/UsdPathFactory.h"
 #include "sceneIO/write/usd/UsdRenderSettingsWriter.h"
 #include "sceneIO/usd/UsdUtils.h"
 #include <pxr/usd/usd/stage.h>
@@ -9,7 +8,6 @@ namespace crayg {
 
 TEST_CASE("UsdRenderSettingsWriter::write") {
     auto stage = pxr::UsdStage::CreateInMemory();
-    UsdPathFactory usdPathFactory;
 
     SECTION("should write rendersettings correctly") {
         RenderSettings renderSettings(Resolution(1280, 720), 4);
