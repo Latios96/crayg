2fb618d3ffea850716a084066de153c6dfb4e9e9
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-06-04 15:59:00 +0200

    feat: throw exception when attribute reading goes wrong

--- src/crayg/src/sceneIO/usd/UsdUtils.h
+++ src/crayg/src/sceneIO/usd/UsdUtils.h
@@ -5,6 +5,7 @@
 #ifndef CRAYG_SRC_CRAYGUSD_SRC_USDUTILS_H_
 #define CRAYG_SRC_CRAYGUSD_SRC_USDUTILS_H_
 
+#include "CraygUsdBase.h"
 #include <pxr/usd/usd/attribute.h>
 
 namespace crayg {
@@ -15,8 +16,10 @@ class UsdUtils {
     template<typename T>
     static T getAttributeValueAs(const pxr::UsdAttribute attribute) {
         T value;
-        attribute.Get(&value);
-        return value;
+        if (attribute.Get(&value)) {
+            return value;
+        }
+        throw std::runtime_error("There was no attribute value to read or attribute was not of the type requested");
     };
 
 };
---
+++ src/crayg/tests/TestUsdUtils.cpp
@@ -0,0 +1,29 @@
+//
+// Created by Jan on 04.06.2022.
+//
+#include <catch2/catch.hpp>
+#include "sceneIO/usd/UsdUtils.h"
+#include <pxr/usd/usd/stage.h>
+#include <pxr/usd/usdGeom/sphere.h>
+
+namespace crayg {
+
+TEST_CASE("UsdUtils::getAttributeValueAs") {
+
+    auto stage = pxr::UsdStage::CreateInMemory();
+    auto sphere = pxr::UsdGeomSphere::Define(stage, pxr::SdfPath("/sphere"));
+    sphere.GetRadiusAttr().Set(3.0);
+
+    SECTION("should read attribute value") {
+        auto radius = UsdUtils::getAttributeValueAs<double>(sphere.GetRadiusAttr());
+
+        REQUIRE(radius == 3.0);
+    }
+
+    SECTION("should throw exception when there is an error reading the attribute") {
+        REQUIRE_THROWS_AS(UsdUtils::getAttributeValueAs<float>(sphere.GetRadiusAttr()), std::runtime_error);
+    }
+
+}
+
+}
