02f28680edba7003848a72631bda839e9ea874ac
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-06-11 15:52:54 +0200

    feat: add per-vertex implementation of TriangleMeshPrimVar

---
+++ src/crayg/src/scene/TriangleMeshPerVertexPrimVar.h
@@ -0,0 +1,40 @@
+#ifndef CRAYG_SRC_CRAYG_SRC_SCENE_TRIANGLEMESHPERVERTEXPRIMVAR_H_
+#define CRAYG_SRC_CRAYG_SRC_SCENE_TRIANGLEMESHPERVERTEXPRIMVAR_H_
+
+#include "TriangleMeshAbstractPrimVar.h"
+
+namespace crayg {
+
+template<typename T>
+struct VertexData {
+    T v0, v1, v2;
+    VertexData() = default;
+    explicit VertexData(const T &value) : v0(value), v1(value), v2(value) {}
+    VertexData(const T &v0, const T &v1, const T &v2) : v0(v0), v1(v1), v2(v2) {}
+};
+
+template<typename T>
+class TriangleMeshPerVertexPrimVar : public TriangleMeshAbstractPrimVar<T> {
+ public:
+    explicit TriangleMeshPerVertexPrimVar(TriangleMesh &triangleMesh) : TriangleMeshAbstractPrimVar<T>(
+        triangleMesh) {}
+    void allocate() override {
+        vertexData.resize(this->triangleMesh.faceCount());
+    }
+    void write(int faceIndex, const VertexData<T> &value) {
+        vertexData[faceIndex] = value;
+    }
+    void write(int faceIndex, const T &v0, const T &v1, const T &v2) {
+        write(faceIndex, {v0, v1, v2});
+    }
+    T interpolateAt(int faceIndex, const Vector3f &point) override {
+        BarycentricCoordinates coordinates = BarycentricCoordinates(Triangle(&this->triangleMesh, faceIndex), point);
+        VertexData<T> vertexDataForFace = vertexData[faceIndex];
+        return coordinates.interpolateLinear(vertexDataForFace.v0, vertexDataForFace.v1, vertexDataForFace.v2);
+    }
+ private:
+    std::vector<VertexData<T>> vertexData;
+};
+}
+
+#endif //CRAYG_SRC_CRAYG_SRC_SCENE_TRIANGLEMESHPERVERTEXPRIMVAR_H_
---
+++ src/crayg/tests/TestTriangleMeshPerVertexPrimVar.cpp
@@ -0,0 +1,37 @@
+#include <catch2/catch.hpp>
+#include "scene/TriangleMesh.h"
+#include "scene/TriangleMeshPerVertexPrimVar.h"
+
+namespace crayg {
+
+TEST_CASE("TriangleMeshPerVertexPrimVar::interpolateAt")
+{
+    TriangleMesh triangleMesh;
+    triangleMesh.points = std::vector<Vector3f>({{-0.5, 0, 0.5}, {0.5, 0, 0.5}, {-0.5, 0, -0.5}, {0.5, 0, -0.5}});
+    triangleMesh.faceIndices = std::vector<int>({0, 2, 1, 2, 3, 1});
+
+    SECTION("should interpolate constant value correctly") {
+        TriangleMeshPerVertexPrimVar<Color> primVar(triangleMesh);
+        primVar.allocate();
+        primVar.write(0, VertexData(Color::createGrey(0.5)));
+        primVar.write(1, VertexData(Color::createGrey(0.5)));
+
+        Color interpolatedValue = primVar.interpolateAt(0, {-0.25, 0, 0.25});
+
+        REQUIRE(interpolatedValue == Color::createGrey(0.5));
+    }
+
+    SECTION("should interpolate non-constant value correctly") {
+        TriangleMeshPerVertexPrimVar<Color> primVar(triangleMesh);
+        primVar.allocate();
+        primVar.write(0, VertexData(Color(1, 0, 0), Color(0, 1, 0), Color(0, 0, 1)));
+        primVar.write(1, VertexData(Color(0, 0, 0), Color(0, 1, 0), Color(1, 0, 0)));
+
+        Color interpolatedValue = primVar.interpolateAt(0, {-0.25, 0, 0.25});
+
+        REQUIRE(interpolatedValue == Color(0.5f, 0.25f, 0.25f));
+    }
+
+}
+
+}
