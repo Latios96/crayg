316fa646a801eb8ef0396add912504716de1771f
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-06-11 15:14:49 +0200

    refactor: cleanup TestTriangleMesh

--- src/crayg/tests/TestTriangleMesh.cpp
+++ src/crayg/tests/TestTriangleMesh.cpp
@@ -1,36 +1,38 @@
-//
-// Created by Jan Honsbrok on 15.11.18.
-//
 #include <catch2/catch.hpp>
 #include <scene/TriangleMesh.h>
 
 namespace crayg {
 
-TEST_CASE("TriangleMesh") {
+TEST_CASE("TriangleMesh::intersect") {
     TriangleMesh cube;
     TriangleMesh::createCube(cube);
 
-    SECTION("intersect_") {
+    SECTION("should not intersect with ray") {
         Ray ray({1.1f, 1.1f, -2}, {0, 0, 1});
+
         const Imageable::Intersection intersection = cube.intersect(ray);
+
         Vector3f hitLocation = ray.constructIntersectionPoint(intersection.rayParameter);
         REQUIRE_FALSE(hitLocation.isValid());
     }
 }
 
-TEST_CASE("TriangleMeshGetBounds") {
-    TriangleMesh triangleMesh;
-    triangleMesh.points.emplace_back(0, 0, 0);
-    triangleMesh.points.emplace_back(0, 1, 0);
-    triangleMesh.points.emplace_back(1, 0, 0);
-    triangleMesh.faceIndices.push_back(0);
-    triangleMesh.faceIndices.push_back(1);
-    triangleMesh.faceIndices.push_back(2);
-    triangleMesh.init();
+TEST_CASE("TriangleMesh::getBounds") {
 
-    const BoundingBox boundingBox = triangleMesh.getBounds();
+    SECTION("bounds should be calculated corretly") {
+        TriangleMesh triangleMesh;
+        triangleMesh.points.emplace_back(0, 0, 0);
+        triangleMesh.points.emplace_back(0, 1, 0);
+        triangleMesh.points.emplace_back(1, 0, 0);
+        triangleMesh.faceIndices.push_back(0);
+        triangleMesh.faceIndices.push_back(1);
+        triangleMesh.faceIndices.push_back(2);
+        triangleMesh.init();
 
-    REQUIRE(boundingBox == BoundingBox({0, 0, 0}, {1, 1, 0}));
+        const BoundingBox boundingBox = triangleMesh.getBounds();
+
+        REQUIRE(boundingBox == BoundingBox({0, 0, 0}, {1, 1, 0}));
+    }
 }
 
 }
