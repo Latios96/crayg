63888bde7db0f3de1d50db9ea5437da5c44fd59d
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-06-24 23:31:40 +0200

    feat: add fmt formatting

--- src/crayg/src/integrators/IntegratorType.h
+++ src/crayg/src/integrators/IntegratorType.h
@@ -2,10 +2,25 @@
 #ifndef CRAYG_SRC_CRAYG_SRC_INTEGRATORS_INTEGRATORTYPE_H_
 #define CRAYG_SRC_CRAYG_SRC_INTEGRATORS_INTEGRATORTYPE_H_
 
+#include <magic_enum.hpp>
+
 namespace crayg {
 
 enum class IntegratorType { RAYTRACING = 1, DEBUG = 2 };
 
 }
+
+template<>
+struct fmt::formatter<crayg::IntegratorType> {
+    template<typename ParseContext>
+    constexpr auto parse(ParseContext &ctx) {
+        return ctx.begin();
+    }
+
+    template<typename FormatContext>
+    auto format(crayg::IntegratorType const &integratorType, FormatContext &ctx) {
+        return fmt::format_to(ctx.out(), magic_enum::enum_name(integratorType));
+    };
+};
 
 #endif //CRAYG_SRC_CRAYG_SRC_INTEGRATORS_INTEGRATORTYPE_H_
