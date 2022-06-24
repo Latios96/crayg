a2d8fcd389a0e8b866f5d47b42d7e72da7ed84b0
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-06-24 20:48:44 +0200

    chore: update build script

--- build_vs_2017.bat
+++
@@ -1,7 +0,0 @@
-mkdir build
-cd build
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-C:\cmake\bin\cmake -G "Ninja" .. -DCMAKE_BUILD_TYPE=Release
-C:\cmake\bin\cmake --build . --config Release
-ctest -C Release -V
-C:\cmake\bin\cmake --build . --config Release --target createDistribution
