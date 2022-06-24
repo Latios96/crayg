30bcf4f74fc716a92ea58763b08ee75f96fe4572
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-06-24 21:34:13 +0200

    feat: add integrator interface

---
+++ src/crayg/src/integrators/AbstractIntegrator.cpp
@@ -0,0 +1,8 @@
+#include "AbstractIntegrator.h"
+
+namespace crayg {
+AbstractIntegrator::AbstractIntegrator(Scene &scene, const std::shared_ptr<SceneIntersector> &sceneIntersector) : scene(
+    scene), sceneIntersector(
+    sceneIntersector) {}
+AbstractIntegrator::~AbstractIntegrator() = default;
+} // crayg
---
+++ src/crayg/src/integrators/AbstractIntegrator.h
@@ -0,0 +1,22 @@
+#ifndef CRAYG_SRC_CRAYG_SRC_INTEGRATORS_ABSTRACTINTEGRATOR_H_
+#define CRAYG_SRC_CRAYG_SRC_INTEGRATORS_ABSTRACTINTEGRATOR_H_
+
+#include "basics/Color.h"
+#include "basics/Ray.h"
+#include "intersectors/SceneIntersector.h"
+
+namespace crayg {
+
+class AbstractIntegrator {
+ public:
+    explicit AbstractIntegrator(Scene &scene, const std::shared_ptr<SceneIntersector> &sceneIntersector);
+    virtual Color integrate(const Ray &ray) = 0;
+    virtual ~AbstractIntegrator();
+ protected:
+    Scene &scene;
+    std::shared_ptr<SceneIntersector> sceneIntersector;
+};
+
+} // crayg
+
+#endif //CRAYG_SRC_CRAYG_SRC_INTEGRATORS_ABSTRACTINTEGRATOR_H_
