c4489a346a13803ea803e60f69a03e44c24c69bd
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-06-26 17:39:45 +0200

    feat: add integration test scenes for lights

---
+++ src/crayg/integrationTests/Lights/render_all.bat
@@ -0,0 +1,6 @@
+C:/workspace/crayg/cmake-build-release-visual-studio/bin/crayg-standalone -s SinglePointLight/SinglePointLight.usda -o SinglePointLight/SinglePointLight.png --maxSamples 4
+C:/workspace/crayg/cmake-build-release-visual-studio/bin/crayg-standalone -s MultiplePointLights/MultiplePointLights.usda -o MultiplePointLights/MultiplePointLights.png --maxSamples 4
+C:/workspace/crayg/cmake-build-release-visual-studio/bin/crayg-standalone -s MultipleColoredPointLights/MultipleColoredPointLights.usda -o MultipleColoredPointLights/MultipleColoredPointLights.png --maxSamples 4
+C:/workspace/crayg/cmake-build-release-visual-studio/bin/crayg-standalone -s SingleRectLight/SingleRectLight.usda -o SingleRectLight/SingleRectLight.png --maxSamples 4
+C:/workspace/crayg/cmake-build-release-visual-studio/bin/crayg-standalone -s SingleDiskLight/SingleDiskLight.usda -o SingleDiskLight/SingleDiskLight.png --maxSamples 4
+
