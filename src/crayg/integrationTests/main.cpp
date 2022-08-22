2faf80fc5f02f43a221a71ae67f4c8a9aba21c64
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-08-22 12:10:18 +0200

    refactor: remove tests that are already covered by cato

--- src/crayg/integrationTests/main.cpp
+++ src/crayg/integrationTests/main.cpp
@@ -1,19 +1,14 @@
 #include "Logger.h"
 #include "KnipserApp.h"
 #include "imageTests.h"
-#include "renderTests.h"
-#include "renderTestsCato.h"
 #include "sceneIO.h"
 
 int main(int argc, char **argv) {
     crayg::Logger::initialize();
 
     knipser::TestRegistry testRegistry;
-    testRegistry
-        .registerTests(crayg::sceneIoTests())
-        .registerTests(crayg::imageTests())
-        .registerTests(crayg::renderTests())
-        .registerTests(crayg::renderTestsCato());
+    testRegistry.registerTests(crayg::sceneIoTests())
+        .registerTests(crayg::imageTests());
 
     KNIPSER_MAIN(testRegistry);
 }
