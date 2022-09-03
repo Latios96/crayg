fee15f8b5a252cadd19141bdfda83ff06760fec9
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-09-03 14:43:55 +0200

    feat: add Gradient class

---
+++ src/crayg/src/basics/Gradient.h
@@ -0,0 +1,78 @@
+#ifndef CRAYG_SRC_CRAYG_SRC_BASICS_GRADIENT_H_
+#define CRAYG_SRC_CRAYG_SRC_BASICS_GRADIENT_H_
+
+#include <vector>
+#include <fmt/format.h>
+#include <ostream>
+#include "Color.h"
+#include "utils/ToStringHelper.h"
+
+namespace crayg {
+
+template<typename T>
+struct GradientStop {
+    GradientStop(float position, T value) : position(position), value(value) {}
+    float position;
+    T value;
+    bool operator==(const GradientStop &rhs) const {
+        return position == rhs.position &&
+            value == rhs.value;
+    }
+    bool operator!=(const GradientStop &rhs) const {
+        return !(rhs == *this);
+    }
+    friend std::ostream &operator<<(std::ostream &os, const GradientStop &stop) {
+        os << ToStringHelper("GradientStop").addMember("position", stop.position).addMember("value",
+                                                                                            stop.value).finish();
+        return os;
+    }
+};
+
+template<typename T>
+class Gradient {
+ public:
+    explicit Gradient(const std::vector<T> &values) {
+        const float stepSize = 1.0f / static_cast<float>(values.size() - 1);
+        for (int i = 0; i < values.size(); i++) {
+            float position = stepSize * i;
+            if (i == 0) {
+                position = 0;
+            } else if (i == values.size() - 1) {
+                position = 1;
+            }
+            stops.emplace_back(position, values[i]);
+        }
+    }
+    explicit Gradient(const std::vector<GradientStop<T>> &stops) : stops(stops) {}
+    T interpolate(float position);
+ private:
+    T lerp(const T &firstValue, const T &secondValue, float position);
+    std::vector<GradientStop<T>> stops;
+};
+
+template<typename T>
+T Gradient<T>::interpolate(float position) {
+    for (int i = 1; i < stops.size(); i++) {
+        GradientStop<T> &firstStop = stops[i - 1];
+        GradientStop<T> &secondStop = stops[i];
+
+        const bool firstValueIsBeforePosition = firstStop.position <= position;
+        const bool secondValueIsAfterPosition = secondStop.position >= position;
+        const bool positionIsBetweenStops = firstValueIsBeforePosition && secondValueIsAfterPosition;
+
+        if (positionIsBetweenStops) {
+            const float positionInInterval = position - firstStop.position;
+            const float relativePositionInInterval = positionInInterval / (secondStop.position - firstStop.position);
+            return lerp(firstStop.value, secondStop.value, relativePositionInInterval);
+        }
+    }
+    throw std::runtime_error(fmt::format("Did not find a stop for position {}", position));
+}
+template<typename T>
+T Gradient<T>::lerp(const T &firstValue, const T &secondValue, float position) {
+    return firstValue * (1 - position) + secondValue * position;
+}
+
+} // crayg
+
+#endif //CRAYG_SRC_CRAYG_SRC_BASICS_GRADIENT_H_
---
+++ src/crayg/tests/TestGradient.cpp
@@ -0,0 +1,57 @@
+#include <catch2/catch.hpp>
+#include "basics/Color.h"
+#include "basics/Gradient.h"
+
+namespace crayg {
+
+TEST_CASE("Gradient::interpolate") {
+
+    SECTION("should interpolate correctly for gradient with two values") {
+        auto testData = GENERATE(table<float, Color>({{0.0f, Color::createBlack()},
+                                                      {0.5f, Color::createGrey(0.5f)},
+                                                      {1.0f, Color::createWhite()}}));
+        const auto position = std::get<0>(testData);
+        const auto expectedValue = std::get<1>(testData);
+        Gradient<Color> gradient({Color::createBlack(), Color::createWhite()});
+
+        const auto result = gradient.interpolate(position);
+
+        REQUIRE(result == expectedValue);
+    }
+
+    SECTION("should interpolate correctly for gradient with linearly distributed values") {
+        auto testData = GENERATE(table<float, Color>({{0.0f, Color::createBlack()},
+                                                      {0.2f, Color::createGrey(0.2f)},
+                                                      {0.4f, Color::createGrey(0.4f)},
+                                                      {0.6f, Color::createGrey(0.6f)},
+                                                      {0.8f, Color::createGrey(0.8f)},
+                                                      {1.0f, Color::createWhite()}}));
+        const auto position = std::get<0>(testData);
+        const auto expectedValue = std::get<1>(testData);
+        Gradient<Color> gradient
+            ({Color::createBlack(), Color::createGrey(0.2f), Color::createGrey(0.4f), Color::createGrey(0.6f),
+              Color::createGrey(0.8f), Color::createWhite()});
+
+        const auto result = gradient.interpolate(position);
+
+        REQUIRE(result == expectedValue);
+    }
+
+    SECTION("should interpolate correctly for gradient with uneven distributed values") {
+        auto testData = GENERATE(table<float, Color>({{0.0f, Color::createBlack()},
+                                                      {0.25f, Color::createGrey(0.1f)},
+                                                      {0.5f, Color::createGrey(0.2f)},
+                                                      {0.75f, Color::createGrey(0.6f)},
+                                                      {1.0f, Color::createWhite()}}));
+        const auto position = std::get<0>(testData);
+        const auto expectedValue = std::get<1>(testData);
+        Gradient<Color> gradient({GradientStop(0.0f, Color::createBlack()), GradientStop(0.5f, Color::createGrey(0.2f)),
+                                  GradientStop(1.0f, Color::createWhite())});
+
+        const auto result = gradient.interpolate(position);
+
+        REQUIRE(result == expectedValue);
+    }
+}
+
+}
