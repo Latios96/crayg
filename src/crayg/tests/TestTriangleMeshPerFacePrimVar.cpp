0278985788bd803c3cc23db2c3d5679a81c88bca
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-09-07 16:20:31 +0200

    feat: add primvar implementation for per-face data

---
+++ src/crayg/tests/TestTriangleMeshPerFacePrimVar.cpp
@@ -0,0 +1,65 @@
+#include <catch2/catch.hpp>
+#include "scene/primitives/trianglemesh/TriangleMesh.h"
+#include "scene/primitives/trianglemesh/primvars/TriangleMeshPerFacePrimVar.h"
+#include "fixtures/TriangleMeshFixtures.h"
+
+namespace crayg {
+
+TEST_CASE("TriangleMeshPerFacePrimVar::interpolateAt")
+{
+    TriangleMesh triangleMesh = TriangleMeshFixtures::createPrimVarFixtureMesh();
+
+    SECTION("should interpolate constant value correctly") {
+        TriangleMeshPerFacePrimVar<Color> primVar(triangleMesh);
+        primVar.allocate();
+        primVar.write(0, Color::createGrey(0.5));
+        primVar.write(1, Color::createGrey(0.5));
+        primVar.write(2, Color::createGrey(0.5));
+        primVar.write(3, Color::createGrey(0.5));
+
+        Color interpolatedValue = primVar.interpolateAt(0, {-0.25, 0, 0.25});
+
+        REQUIRE(interpolatedValue == Color::createGrey(0.5));
+    }
+
+}
+
+TEST_CASE("TriangleMeshPerFacePrimVar::==") {
+    TriangleMesh triangleMesh = TriangleMeshFixtures::createPrimVarFixtureMesh();
+
+    TriangleMeshPerFacePrimVar<Color> primVar(triangleMesh);
+    primVar.allocate();
+    primVar.write(0, Color(1, 0, 0));
+    primVar.write(1, Color(0, 1, 0));
+    primVar.write(2, Color(0, 0, 1));
+    primVar.write(3, Color(1, 1, 1));
+
+    SECTION("primvar should be equal to equivalent buffer") {
+        REQUIRE(primVar == std::vector<Color>({{1, 0, 0}, {0, 1, 0}, {0, 0, 1}, {1, 1, 1}}));
+    }
+
+    SECTION("primvar should not be equal to different equivalent buffer") {
+        REQUIRE(primVar != std::vector<Color>());
+    }
+}
+
+TEST_CASE("TriangleMeshPerFacePrimVar::apply") {
+    TriangleMesh triangleMesh = TriangleMeshFixtures::createPrimVarFixtureMesh();
+
+    TriangleMeshPerFacePrimVar<Color> primVar(triangleMesh);
+    primVar.allocate();
+    primVar.write(0, Color(1, 0, 0));
+    primVar.write(1, Color(0, 1, 0));
+    primVar.write(2, Color(0, 0, 1));
+    primVar.write(3, Color(1, 1, 1));
+
+    SECTION("should apply function to all elements") {
+        primVar.apply([](Color color) {
+            return color * 2;
+        });
+
+        REQUIRE(primVar == std::vector<Color>({{2, 0, 0}, {0, 2, 0}, {0, 0, 2}, {2, 2, 2}}));
+    }
+}
+
+}
