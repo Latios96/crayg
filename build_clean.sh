c5ed5b55a0d8b70ba86795da11ae00c150e485a1
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-09-07 16:33:54 +0200

    chore: remove deprecated files

--- build_clean.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-rm -rf build/
-mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
-make -j
-ctest -V
-
--- build_vs_2019.bat
+++
@@ -1,7 +0,0 @@
-mkdir build
-cd build
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-cmake -G "Ninja" .. -DCMAKE_BUILD_TYPE=Release
-cmake --build . --config Release
-ctest -C Release -V
-cmake --build . --config Release --target createDistribution
