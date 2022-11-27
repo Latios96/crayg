551f25882d0d924f81473257fc1d66d9bf0a0776
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-11-27 17:47:28 +0100

    fix: settings the radius is irrelevant here

--- src/crayg/tests/TestBaseUsdXformableReader.cpp
+++ src/crayg/tests/TestBaseUsdXformableReader.cpp
@@ -23,7 +23,6 @@ TEST_CASE("BaseUsdXformableReader::read") {
     SECTION("should read prim translation") {
         auto usdSphere = pxr::UsdGeomSphere::Define(stage, pxr::SdfPath("/usdSphere"));
         pxr::UsdGeomXformCommonAPI(usdSphere).SetTranslate(pxr::GfVec3f(1, 2, 3));
-        usdSphere.GetRadiusAttr().Set(3.0); // TODO do we need radius here?
 
         DummyBaseReader dummyBaseReader(usdSphere);
         auto sphere = dummyBaseReader.read();
@@ -34,7 +33,6 @@ TEST_CASE("BaseUsdXformableReader::read") {
     SECTION("should read time sampled prim translation correctly") {
         auto usdSphere = pxr::UsdGeomSphere::Define(stage, pxr::SdfPath("/usdSphere"));
         pxr::UsdGeomXformCommonAPI(usdSphere).SetTranslate(pxr::GfVec3f(1, 2, 3), pxr::UsdTimeCode());
-        usdSphere.GetRadiusAttr().Set(3.0); // TODO do we need radius here?
 
         DummyBaseReader dummyBaseReader(usdSphere);
         auto sphere = dummyBaseReader.read();
