1a74315c6e8755642fd33c976024f203f77374d7
Jan Honsbrok <jan.honsbrok@gmail.com>
2023-01-19 18:03:10 +0100

    refactor: extract methods

--- src/crayg/src/utils/ImageMetadataCollector.cpp
+++ src/crayg/src/utils/ImageMetadataCollector.cpp
@@ -14,42 +14,59 @@ ImageMetadataCollector::ImageMetadataCollector(const std::optional<std::chrono::
 ImageMetadata ImageMetadataCollector::collectMetadata() {
     ImageMetadata imageMetadata;
 
-    imageMetadata.write(ImageMetadataTokens::CRAYG_VERSION,
-                        fmt::format("{}-{}", crayg::CraygInfo::VERSION, crayg::CraygInfo::COMMIT_HASH));
+    collectCraygVersion(imageMetadata);
+    collectCpuName(imageMetadata);
+    collectRenderTimeIfPresent(imageMetadata);
+    collectRenderSettingsIfPresent(imageMetadata);
 
+    return imageMetadata;
+}
+
+void ImageMetadataCollector::collectCraygVersion(ImageMetadata &imageMetadata) const {
+    imageMetadata.write(ImageMetadataTokens::CRAYG_VERSION,
+                        fmt::format("{}-{}", CraygInfo::VERSION, CraygInfo::COMMIT_HASH));
+}
+
+void ImageMetadataCollector::collectCpuName(ImageMetadata &imageMetadata) const {
     cpuinfo_initialize();
     imageMetadata.write(ImageMetadataTokens::CPU, cpuinfo_get_package(0)->name);
+}
 
+void ImageMetadataCollector::collectRenderTimeIfPresent(ImageMetadata &imageMetadata) {
     if (renderTime) {
         imageMetadata.write(ImageMetadataTokens::RENDER_TIME, fmt::format("{:%T}", renderTime.value()));
-        imageMetadata.write(ImageMetadataTokens::RENDER_TIME_SECONDS,  renderTime.value());
+        imageMetadata.write(ImageMetadataTokens::RENDER_TIME_SECONDS, renderTime.value());
     }
+}
 
+void ImageMetadataCollector::collectRenderSettingsIfPresent(ImageMetadata &imageMetadata) {
     if (renderSettings) {
         imageMetadata.write(ImageMetadataTokens::RENDER_SETTINGS_INTEGRATOR,
                             std::string(magic_enum::enum_name(renderSettings.value().integratorType)));
         imageMetadata.write(ImageMetadataTokens::RENDER_SETTINGS_INTERSECTOR,
                             std::string(magic_enum::enum_name(renderSettings.value().intersectorType)));
         imageMetadata.write(ImageMetadataTokens::RENDER_SETTINGS_MAX_SAMPLES, renderSettings.value().maxSamples);
-        for (const auto &entry: renderSettings.value().integratorSettings.settings) {
-            switch (entry.second.index()) {
-                case 0:
-                    imageMetadata.write("crayg/renderSettings/integratorSettings/" + entry.first,
-                                        std::get<std::string>(entry.second));
-                    break;
-                case 1:
-                    imageMetadata.write("crayg/renderSettings/integratorSettings/" + entry.first,
-                                        std::get<int>(entry.second));
-                    break;
-                case 2:
-                    imageMetadata.write("crayg/renderSettings/integratorSettings/" + entry.first,
-                                        std::get<float>(entry.second));
-                    break;
-            }
+        collectIntegratorSettings(imageMetadata);
+    }
+}
+
+void ImageMetadataCollector::collectIntegratorSettings(ImageMetadata &imageMetadata) {
+    for (const auto &entry: renderSettings.value().integratorSettings.settings) {
+        switch (entry.second.index()) {
+            case 0:
+                imageMetadata.write("crayg/renderSettings/integratorSettings/" + entry.first,
+                                    std::get<std::string>(entry.second));
+                break;
+            case 1:
+                imageMetadata.write("crayg/renderSettings/integratorSettings/" + entry.first,
+                                    std::get<int>(entry.second));
+                break;
+            case 2:
+                imageMetadata.write("crayg/renderSettings/integratorSettings/" + entry.first,
+                                    std::get<float>(entry.second));
+                break;
         }
     }
-
-    return imageMetadata;
 }
 
 } // crayg
--- src/crayg/src/utils/ImageMetadataCollector.h
+++ src/crayg/src/utils/ImageMetadataCollector.h
@@ -9,10 +9,18 @@ class ImageMetadataCollector {
     ImageMetadataCollector();
     ImageMetadataCollector(const std::optional<std::chrono::seconds> &renderTime,
                            const std::optional<RenderSettings> &renderSettings);
+
     std::optional<std::chrono::seconds> renderTime;
     std::optional<RenderSettings> renderSettings;
 
     ImageMetadata collectMetadata();
+
+ private:
+    void collectCraygVersion(ImageMetadata &imageMetadata) const;
+    void collectCpuName(ImageMetadata &imageMetadata) const;
+    void collectRenderTimeIfPresent(ImageMetadata &imageMetadata);
+    void collectRenderSettingsIfPresent(ImageMetadata &imageMetadata);
+    void collectIntegratorSettings(ImageMetadata &imageMetadata);
 };
 
 } // crayg
