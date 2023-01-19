4befa3055f302d43bd033389b396850f34fc9a9a
Jan Honsbrok <jan.honsbrok@gmail.com>
2023-01-19 17:07:54 +0100

    feat: write image metadata

--- src/crayg/src/image/OpenImageIoImageWriter.cpp
+++ src/crayg/src/image/OpenImageIoImageWriter.cpp
@@ -3,15 +3,33 @@
 
 namespace crayg {
 
+
+
 void OpenImageIoImageWriter::writeImage(const Image &image, std::string image_name) {
     std::unique_ptr<OIIO::ImageOutput> out = OIIO::ImageOutput::create(image_name);
     if (!out)
         return;
 
     OIIO::ImageSpec spec(image.getWidth(), image.getHeight(), RGB_CHANNEL_COUNT, OIIO::TypeDesc::FLOAT);
+
+    writeImageMetadata(image, spec);
+
     out->open(image_name, spec);
     out->write_image(OIIO::TypeDesc::FLOAT, image.getValues());
     out->close();
+}
+void OpenImageIoImageWriter::writeImageMetadata(const Image &image, OIIO::ImageSpec &spec) {
+    for(auto &metadata : image.metadata){
+        if (std::holds_alternative<std::string>(metadata.second)) {
+            spec.attribute(metadata.first,std::get<std::string>(metadata.second));
+        }else if (std::holds_alternative<int>(metadata.second)) {
+            spec.attribute(metadata.first,std::get<int>(metadata.second));
+        } else if (std::holds_alternative<float>(metadata.second)) {
+            spec.attribute(metadata.first,std::get<float>(metadata.second));
+        } else if (std::holds_alternative<std::chrono::seconds>(metadata.second)) {
+            spec.attribute(metadata.first,static_cast<unsigned int>(std::get<std::chrono::seconds>(metadata.second).count()));
+        }
+    }
 }
 
 OpenImageIoImageWriter::~OpenImageIoImageWriter() = default;
--- src/crayg/src/image/OpenImageIoImageWriter.h
+++ src/crayg/src/image/OpenImageIoImageWriter.h
@@ -1,6 +1,7 @@
 #ifndef CRAYG_SRC_CRAYG_SRC_IMAGE_OPENIMAGEIOIMAGEWRITER_H_
 #define CRAYG_SRC_CRAYG_SRC_IMAGE_OPENIMAGEIOIMAGEWRITER_H_
 
+#include <OpenImageIO/imageio.h>
 #include "ImageWriter.h"
 
 namespace crayg {
@@ -12,6 +13,8 @@ class OpenImageIoImageWriter : public ImageWriter {
     void writeImage(const Image &image, std::string image_name) override;
 
     virtual ~OpenImageIoImageWriter();
+ private:
+    void writeImageMetadata(const Image &image, OIIO::ImageSpec &spec);
 };
 
 }
