4e72a14aa4a6abc0706386c7d8846d3c9c16086f
Jan Honsbrok <jan.honsbrok@gmail.com>
2023-01-20 15:20:14 +0100

    test: add tests for EmbreeBvhBuilder

---
+++ src/crayg/tests/TestEmbreeBvhBuilder.cpp
@@ -0,0 +1,25 @@
+#include <catch2/catch.hpp>
+#include "fixtures/TriangleMeshFixtures.h"
+#include "intersectors/embree/EmbreeBvhBuilder.h"
+
+namespace crayg {
+
+TEST_CASE("EmbreeBvhBuilder::build") {
+
+    SECTION("should support all supported geometry types") {
+        Scene scene;
+        TriangleMesh triangleMesh = TriangleMeshFixtures::createPrimVarFixtureMesh();
+        scene.addObject(std::shared_ptr<TriangleMesh>(&triangleMesh));
+        auto sphere = std::make_shared<Sphere>(Vector3f(0.f, 1.f, 0.f), 1.f);
+        scene.addObject(sphere);
+
+        EmbreeBvhBuilder embreeBvhBuilder(scene);
+        auto embreeBvh = embreeBvhBuilder.build();
+
+        REQUIRE(embreeBvh->geomIdToSceneObject.size() == 2);
+        REQUIRE(embreeBvh->geomIdToSceneObject[0] == std::make_pair(0u, EmbreePrimitiveType::TRIANGLE_MESH));
+        REQUIRE(embreeBvh->geomIdToSceneObject[1] == std::make_pair(1u, EmbreePrimitiveType::SPHERE));
+    }
+}
+
+}
