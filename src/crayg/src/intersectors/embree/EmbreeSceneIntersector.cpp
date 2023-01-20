b908b05521360abcd5da7f682cc30500f7beff7a
Jan Honsbrok <jan.honsbrok@gmail.com>
2023-01-20 14:23:36 +0100

    fix: add small epsilon to Embree rays to prevent shadow akne
    Embree seems to be much more prone to such errors than the naive BVH implementation

--- src/crayg/src/intersectors/embree/EmbreeSceneIntersector.cpp
+++ src/crayg/src/intersectors/embree/EmbreeSceneIntersector.cpp
@@ -10,7 +10,7 @@ Imageable::Intersection EmbreeSceneIntersector::intersect(const Ray &ray) const 
     rtcRayHit.ray.org_x = ray.startPoint.x;
     rtcRayHit.ray.org_y = ray.startPoint.y;
     rtcRayHit.ray.org_z = ray.startPoint.z;
-    rtcRayHit.ray.tnear = 0.f;
+    rtcRayHit.ray.tnear = 0.001f;
     rtcRayHit.ray.dir_x = ray.direction.x;
     rtcRayHit.ray.dir_y = ray.direction.y;
     rtcRayHit.ray.dir_z = ray.direction.z;
@@ -39,7 +39,7 @@ bool EmbreeSceneIntersector::isIntersecting(const Ray &ray) const {
     rtcRayHit.ray.org_x = ray.startPoint.x;
     rtcRayHit.ray.org_y = ray.startPoint.y;
     rtcRayHit.ray.org_z = ray.startPoint.z;
-    rtcRayHit.ray.tnear = 0.f;
+    rtcRayHit.ray.tnear = 0.001f;
     rtcRayHit.ray.dir_x = ray.direction.x;
     rtcRayHit.ray.dir_y = ray.direction.y;
     rtcRayHit.ray.dir_z = ray.direction.z;
