32217c78748a5131db9a75499e891450e8248f09
Jan Honsbrok <jan.honsbrok@gmail.com>
2023-01-23 19:35:45 +0100

    feat: add CRAYG_PAIR_DTO macro for easy dto generation

---
+++ src/crayg/src/utils/PairDto.h
@@ -0,0 +1,35 @@
+#ifndef CRAYG_SRC_CRAYG_SRC_UTILS_PAIRDTO_H_
+#define CRAYG_SRC_CRAYG_SRC_UTILS_PAIRDTO_H_
+
+#include <string>
+#include <ostream>
+#include "ToStringHelper.h"
+
+#define CRAYG_PAIR_DTO(Name, FirstType, FirstName, SecondType, SecondName) \
+struct Name {\
+    FirstType FirstName;                                                   \
+    SecondType SecondName;                                                 \
+                                                                           \
+    Name() = default;                                                      \
+                                                                           \
+    Name(FirstType FirstName,SecondType SecondName):FirstName(FirstName),SecondName(SecondName){} \
+                                                                           \
+    Name(const Name &name):FirstName(name.FirstName),SecondName(name.SecondName){} \
+                                                                           \
+    bool operator==(const Name &rhs) const { \
+        return FirstName == rhs.FirstName && \
+            SecondName == rhs.SecondName; \
+    } \
+    bool operator!=(const Name &rhs) const { \
+        return !(rhs == *this); \
+    } \
+    friend std::ostream &operator<<(std::ostream &os, const Name &dto) { \
+        os << ToStringHelper("Name") \
+            .addMember("FirstName", dto.FirstName) \
+            .addMember("SecondName", dto.SecondName) \
+            .finish(); \
+        return os; \
+    }\
+};\
+
+#endif //CRAYG_SRC_CRAYG_SRC_UTILS_PAIRDTO_H_
