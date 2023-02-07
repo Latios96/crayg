9b652ee9a4abe961c38f4bbdaa8ba5ac270d8881
Jan Honsbrok <jan.honsbrok@gmail.com>
2023-02-07 20:27:58 +0100

    feat: add ProgressReporter for geometry compilation

--- src/crayg/src/renderer/GeometryCompiler.cpp
+++ src/crayg/src/renderer/GeometryCompiler.cpp
@@ -1,25 +1,29 @@
 #include <tbb/parallel_for.h>
 #include "GeometryCompiler.h"
 #include "scene/primitives/subdivisionsurfacemesh/SubdivisionSurfaceMesh.h"
-#include "utils/StopWatch.h"
+#include "utils/ProgressReporter.h"
 
 namespace crayg {
 
 GeometryCompiler::GeometryCompiler(Scene &scene) : scene(scene) {}
 
 void GeometryCompiler::compile() {
-    InformativeScopedStopWatch buildBvh("Compiling geometry");
     tessellateSubdivisionSurfaces();
 }
 void GeometryCompiler::tessellateSubdivisionSurfaces() {
     std::vector<std::shared_ptr<SubdivisionSurfaceMesh>> subdivisionSurfaces;
     findAllSubdivisionSurfaceMeshes(subdivisionSurfaces);
 
+    ProgressReporter reporter = ProgressReporter::createLoggingProgressReporter(static_cast<int>(subdivisionSurfaces.size()),
+                                                                                "Compiling geometry");
+
     tbb::parallel_for(static_cast<std::size_t>(0),
                       subdivisionSurfaces.size(),
-                      [&subdivisionSurfaces](int i) {
+                      [&subdivisionSurfaces, &reporter](int i) {
                           subdivisionSurfaces[i]->tessellate();
+                          reporter.iterationDone();
                       });
+    reporter.finish();
 }
 void GeometryCompiler::findAllSubdivisionSurfaceMeshes(std::vector<std::shared_ptr<SubdivisionSurfaceMesh>> &subdivisionSurfaces) {
     for(auto &sceneObject : scene.objects){
