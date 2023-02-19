86f6d6e3e8ffcf70f6ffb713ba440f572a34fc2a
Jan Honsbrok <jan.honsbrok@gmail.com>
2023-02-19 14:27:27 +0100

    feat: register ImageSpec as QMetaType

--- src/standalonegui/widgets/QtMetaTypes.h
+++ src/standalonegui/widgets/QtMetaTypes.h
@@ -3,17 +3,20 @@
 
 #include <QMetaType>
 #include <image/ImageMetadata.h>
+#include <image/ImageSpec.h>
 
 Q_DECLARE_METATYPE(crayg::ImageMetadata);
 Q_DECLARE_METATYPE(crayg::ImageBucket);
 Q_DECLARE_METATYPE(crayg::BucketImageBuffer);
 Q_DECLARE_METATYPE(std::shared_ptr<crayg::BucketImageBuffer>);
+Q_DECLARE_METATYPE(crayg::ImageSpec);
 
-void registerQMetaTypes(){
+void registerQMetaTypes() {
     qRegisterMetaType<crayg::ImageMetadata>("ImageMetadata");
     qRegisterMetaType<crayg::ImageBucket>("ImageBucket");
     qRegisterMetaType<crayg::BucketImageBuffer>();
     qRegisterMetaType<std::shared_ptr<crayg::BucketImageBuffer>>("std::shared_ptr<BucketImageBuffer>");
+    qRegisterMetaType<crayg::ImageSpec>("ImageSpec");
 }
 
 #endif //CRAYG_SRC_STANDALONEGUI_WIDGETS_QTMETATYPES_H_
