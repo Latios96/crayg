3a076184d6bb70b22e9fb40180c82ed597b77ea7
Jan Honsbrok <jan.honsbrok@gmail.com>
2023-02-19 14:56:03 +0100

    feat: add ImageSpecBuilder

--- src/crayg/src/image/ImageSpec.h
+++ src/crayg/src/image/ImageSpec.h
@@ -12,6 +12,47 @@ CRAYG_DTO_3(ChannelSpec, std::string, name, PixelFormat, pixelFormat, int, color
 
 CRAYG_DTO_2(ImageSpec, Resolution, resolution, std::vector<ChannelSpec>, channels) ;
 
+class ImageSpecBuilder {
+ public:
+    ImageSpecBuilder(const Resolution &resolution) {
+        imageSpec = ImageSpec(resolution, {{"rgb", PixelFormat::FLOAT, 3}});
+    }
+    ImageSpecBuilder &addAlphaChannel() {
+        createGreyFloatChannel("alpha");
+        return *this;
+    }
+
+    ImageSpecBuilder &addDepthChannel() {
+        createGreyFloatChannel("depth");
+        return *this;
+    }
+
+    ImageSpecBuilder &createRgbFloatChannel(const std::string &name) {
+        imageSpec.channels.emplace_back(name, PixelFormat::FLOAT, 3);
+        return *this;
+    }
+    ImageSpecBuilder &createGreyFloatChannel(const std::string &name) {
+        imageSpec.channels.emplace_back(name, PixelFormat::FLOAT, 1);
+        return *this;
+    }
+    ImageSpecBuilder &createRgbUInt8Channel(const std::string &name) {
+        imageSpec.channels.emplace_back(name, PixelFormat::UINT8, 3);
+        return *this;
+    }
+    ImageSpecBuilder &createGreyUInt8Channel(const std::string &name) {
+        imageSpec.channels.emplace_back(name, PixelFormat::UINT8, 1);
+        return *this;
+    }
+    ImageSpecBuilder &createVector3fChannel(const std::string &name) {
+        imageSpec.channels.emplace_back(name, PixelFormat::FLOAT, 3);
+        return *this;
+    }
+
+    ImageSpec finish() { return imageSpec; }
+ private:
+    ImageSpec imageSpec;
+};
+
 } // crayg
 
 #endif //CRAYG_SRC_CRAYG_SRC_IMAGE_IMAGESPEC_H_
