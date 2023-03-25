8d792a6db346babf63da5a7366bd14c742664670
Jan Honsbrok <jan.honsbrok@gmail.com>
2023-03-25 20:12:06 +0100

    feat: add concentricSampleDisk

--- src/crayg/src/sampling/Sampling.cpp
+++ src/crayg/src/sampling/Sampling.cpp
@@ -26,4 +26,29 @@ Vector3f Sampling::uniformSampleSphere(float u, float v) {
     float phi = 2.0f * boost::math::constants::pi<float>() * v;
     return {r * std::cos(phi), r * std::sin(phi), z};
 }
+
+Vector2f Sampling::concentricSampleDisk() {
+    return concentricSampleDisk(Random::random(), Random::random());
+}
+
+Vector2f Sampling::concentricSampleDisk(float u, float v) {
+    static float piOver4 = boost::math::constants::pi<float>() / 4;
+    static float piOver2 = boost::math::constants::pi<float>() / 2;
+    float uOffsetX = 2.f * u - 1;
+    float uOffsetY = 2.f * v - 1;
+
+    if (uOffsetX == 0 && uOffsetY == 0) {
+        return {0, 0};
+    }
+
+    float theta, r;
+    if (std::abs(uOffsetX) > std::abs(uOffsetY)) {
+        r = uOffsetX;
+        theta = piOver4 * (uOffsetY / uOffsetX);
+    } else {
+        r = uOffsetY;
+        theta = piOver2 - piOver4 * (uOffsetX / uOffsetY);
+    }
+    return Vector2f(std::cos(theta), std::sin(theta)) * r;
+}
 } // crayg
--- src/crayg/src/sampling/Sampling.h
+++ src/crayg/src/sampling/Sampling.h
@@ -1,6 +1,7 @@
 #ifndef CRAYG_SRC_CRAYG_SRC_SAMPLING_SAMPLING_H_
 #define CRAYG_SRC_CRAYG_SRC_SAMPLING_SAMPLING_H_
 
+#include "basics/Vector2.h"
 #include "basics/Vector3f.h"
 
 namespace crayg {
@@ -11,6 +12,8 @@ class Sampling {
     static Vector3f uniformSampleHemisphere(float u, float v);
     static Vector3f uniformSampleSphere();
     static Vector3f uniformSampleSphere(float u, float v);
+    static Vector2f concentricSampleDisk();
+    static Vector2f concentricSampleDisk(float u, float v);
 };
 
 } // crayg
