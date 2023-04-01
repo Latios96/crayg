18aa7cfaca24b44cbe729a6f85396e885a178058
Jan Honsbrok <jan.honsbrok@gmail.com>
2023-04-01 15:27:07 +0200

    feat: add Bounds2d

---
+++ src/crayg/src/basics/Bound2d.h
@@ -0,0 +1,58 @@
+#ifndef CRAYG_SRC_CRAYG_SRC_BASICS_BOUND2D_H_
+#define CRAYG_SRC_CRAYG_SRC_BASICS_BOUND2D_H_
+
+#include "Vector2.h"
+#include "utils/ToStringHelper.h"
+#include <fmt/ostream.h>
+#include <ostream>
+
+namespace crayg {
+
+template <typename T> class Bounds2d {
+  public:
+    Bounds2d() : min({0}), max({0}) {
+    }
+
+    Bounds2d(const Vector2<T> &min, const Vector2<T> &max) : min(min), max(max) {
+    }
+
+    Bounds2d(const Bounds2d<T> &vec) : min(vec.min), max(vec.max) {
+    }
+
+    Vector2<T> min, max;
+
+    template <typename OT> Bounds2d<T> unionWith(const Bounds2d<OT> &other) {
+        return {{std::min<T>(min.x, static_cast<T>(other.min.x)), std::min<T>(min.y, static_cast<T>(other.min.y))},
+                {std::max<T>(max.x, static_cast<T>(other.max.x)), std::max<T>(max.y, static_cast<T>(other.max.y))}};
+    }
+
+    template <typename OT> Bounds2d<T> unionWith(const Vector2<OT> &other) {
+        return {{std::min<T>(min.x, static_cast<T>(other.x)), std::min<T>(min.y, static_cast<T>(other.y))},
+                {std::max<T>(max.x, static_cast<T>(other.x)), std::max<T>(max.y, static_cast<T>(other.y))}};
+    }
+
+    template <typename OT> Bounds2d<T> expand(const Vector2<OT> &other) {
+        return {min - other, max + other};
+    }
+
+    bool operator==(const Bounds2d &rhs) const {
+        return min == rhs.min && max == rhs.max;
+    }
+
+    bool operator!=(const Bounds2d &rhs) const {
+        return !(rhs == *this);
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const Bounds2d &d) {
+        os << ToStringHelper("Bounds2d").addMember("min", d.min).addMember("max", d.max).finish();
+        return os;
+    }
+};
+
+typedef Bounds2d<float> Bounds2df;
+typedef Bounds2d<int> Bounds2di;
+
+template <typename T> struct fmt::formatter<crayg::Bounds2d<T>> : ostream_formatter {};
+
+}
+#endif // CRAYG_SRC_CRAYG_SRC_BASICS_BOUND2D_H_
---
+++ src/crayg/tests/TestBounds2d.cpp
@@ -0,0 +1,138 @@
+#include "basics/Bound2d.h"
+#include <catch2/catch.hpp>
+#include <sstream>
+
+namespace crayg {
+
+TEST_CASE("Bounds2d::construct") {
+
+    SECTION("should default construct") {
+        Bounds2df bounds2Df;
+
+        REQUIRE(bounds2Df == Bounds2df({0, 0}, {0, 0}));
+    }
+
+    SECTION("should construct with params") {
+        Bounds2df bounds2Df({1, 2}, {3, 4});
+
+        REQUIRE(bounds2Df == Bounds2df({1, 2}, {3, 4}));
+    }
+
+    SECTION("should copy construct") {
+        const Bounds2df bounds2Df({1, 2}, {3, 4});
+        Bounds2df copy = bounds2Df;
+
+        REQUIRE(copy == bounds2Df);
+    }
+}
+
+TEST_CASE("Bounds2d::union with Vector2") {
+    SECTION("Bounds2df should union with Vector2f") {
+        Bounds2df bounds2Df;
+        auto unionBounds = bounds2Df.unionWith(Vector2f(-1, 1));
+
+        REQUIRE(unionBounds == Bounds2df({-1, 0}, {0, 1}));
+    }
+
+    SECTION("Bounds2df should union with Vector2i") {
+        Bounds2df bounds2Df;
+        auto unionBounds = bounds2Df.unionWith(Vector2i(-1, 1));
+
+        REQUIRE(unionBounds == Bounds2df({-1, 0}, {0, 1}));
+    }
+
+    SECTION("Bounds2di should union with Vector2f") {
+        Bounds2di bounds2Df;
+        auto unionBounds = bounds2Df.unionWith(Vector2f(-1, 1));
+
+        REQUIRE(unionBounds == Bounds2di({-1, 0}, {0, 1}));
+    }
+
+    SECTION("Bounds2di should union with Vector2i") {
+        Bounds2di bounds2Df;
+        auto unionBounds = bounds2Df.unionWith(Vector2i(-1, 1));
+
+        REQUIRE(unionBounds == Bounds2di({-1, 0}, {0, 1}));
+    }
+}
+
+TEST_CASE("Bounds2d::union with Bounds") {
+    SECTION("Bounds2df should union with Bounds2df") {
+        Bounds2df bounds2Df;
+        auto unionBounds = bounds2Df.unionWith(Bounds2df({-1, 0}, {0, 1}));
+
+        REQUIRE(unionBounds == Bounds2df({-1, 0}, {0, 1}));
+    }
+
+    SECTION("Bounds2df should union with Bounds2di") {
+        Bounds2df bounds2Df;
+        auto unionBounds = bounds2Df.unionWith(Bounds2di({-1, 0}, {0, 1}));
+
+        REQUIRE(unionBounds == Bounds2df({-1, 0}, {0, 1}));
+    }
+
+    SECTION("Bounds2di should union with Bounds2df") {
+        Bounds2di bounds2Df;
+        auto unionBounds = bounds2Df.unionWith(Bounds2df({-1, 0}, {0, 1}));
+
+        REQUIRE(unionBounds == Bounds2di({-1, 0}, {0, 1}));
+    }
+
+    SECTION("Bounds2di should union with Bounds2di") {
+        Bounds2di bounds2Df;
+        auto unionBounds = bounds2Df.unionWith(Bounds2di({-1, 0}, {0, 1}));
+
+        REQUIRE(unionBounds == Bounds2di({-1, 0}, {0, 1}));
+    }
+}
+
+TEST_CASE("Bounds2d::expand") {
+    SECTION("Bounds2df should expand by Vector2f") {
+        Bounds2df bounds2Df;
+        auto expandedBounds = bounds2Df.expand(Vector2f(1, 2));
+
+        REQUIRE(expandedBounds == Bounds2df({-1, -2}, {1, 2}));
+    }
+}
+
+TEST_CASE("Bounds2d::equality") {
+    SECTION("should be equal") {
+        Bounds2df left;
+        Bounds2df right;
+
+        REQUIRE(left == right);
+    }
+
+    SECTION("different min should not be equal") {
+        Bounds2df left({-1, 0}, {0, 0});
+        Bounds2df right;
+
+        REQUIRE(left != right);
+    }
+
+    SECTION("different max should not be equal") {
+        Bounds2df left({0, 0}, {-1, 0});
+        Bounds2df right;
+
+        REQUIRE(left != right);
+    }
+}
+
+TEST_CASE("Bounds2d::formatting") {
+    SECTION("should be ostreamable") {
+        std::ostringstream os;
+        Bounds2df bounds2Df({1, 2}, {3, 4});
+
+        os << bounds2Df;
+
+        REQUIRE(os.str() == "Bounds2d{min=Vector2{x=1,y=2},max=Vector2{x=3,y=4}}");
+    }
+
+    SECTION("should be formatable") {
+        Bounds2df bounds2Df({1, 2}, {3, 4});
+
+        REQUIRE(fmt::format("{}", bounds2Df) == "Bounds2d{min=Vector2{x=1,y=2},max=Vector2{x=3,y=4}}");
+    }
+}
+
+}
