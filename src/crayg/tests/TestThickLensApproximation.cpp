8fd2601dcc228b977b08a76df84acc88a849dc83
Jan Honsbrok <jan.honsbrok@gmail.com>
2023-04-04 12:58:55 +0200

    tests: add correct values for seconds cardinal point (verified with PBRT)

--- src/crayg/tests/TestThickLensApproximation.cpp
+++ src/crayg/tests/TestThickLensApproximation.cpp
@@ -16,11 +16,11 @@ TEST_CASE("ThickLensApproximationCalculator::calculate") {
         const float focalLength =
             thickLensApproximation.firstCardinalPoints.fZ - thickLensApproximation.firstCardinalPoints.pZ;
         REQUIRE(focalLength == Catch::Detail::Approx(7.21183792f)); // todo extract method for this
-        // todo approx comparison
+
         REQUIRE(thickLensApproximation.firstCardinalPoints.pZ == Catch::Detail::Approx(-7.4172355f));
         REQUIRE(thickLensApproximation.firstCardinalPoints.fZ == Catch::Detail::Approx(-0.20541f));
-        REQUIRE(thickLensApproximation.secondCardinalPoints.pZ == Catch::Detail::Approx(-20.77879f)); // todo check this
-        REQUIRE(thickLensApproximation.secondCardinalPoints.fZ == Catch::Detail::Approx(-27.39604f));
+        REQUIRE(thickLensApproximation.secondCardinalPoints.pZ == Catch::Detail::Approx(-11.7357));
+        REQUIRE(thickLensApproximation.secondCardinalPoints.fZ == Catch::Detail::Approx(-18.94753f));
     }
     // todo check why
     /*SECTION("should throw if aperture is too small") {
