027658f8c381c9707c290c7307d0f65b76c6fa4e
Jan Honsbrok <jan.honsbrok@gmail.com>
2023-04-06 16:45:06 +0200

    feat: add case for tabs

--- src/crayg/tests/TestLensFileTextFileReader.cpp
+++ src/crayg/tests/TestLensFileTextFileReader.cpp
@@ -25,7 +25,12 @@ TEST_CASE("LensFileTextFileReader::readFileContent") {
 # a header comment
 1 2 3 4
 5 6 7 8
-#9 10 11 12)");
+#9 10 11 12)",
+                                    R"(# a header comment
+# a header comment
+1   2   3   4
+5   6   7   8
+#9 10 11    12)");
 
         auto lensElements = lensFileTextFileReader.readFileContent(fileContent);
 
