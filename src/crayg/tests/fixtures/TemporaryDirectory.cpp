7bb34815b7ddc22c8811fa33253dddea9c28fbea
Jan Honsbrok <jan.honsbrok@gmail.com>
2023-04-07 12:37:44 +0200

    feat: add utility to get file name

--- src/crayg/tests/fixtures/TemporaryDirectory.cpp
+++ src/crayg/tests/fixtures/TemporaryDirectory.cpp
@@ -13,16 +13,20 @@ TemporaryDirectory::~TemporaryDirectory() {
     boost::filesystem::remove_all(path);
 }
 
-const boost::filesystem::path &TemporaryDirectory::getPath() const {
+const boost::filesystem::path TemporaryDirectory::getPath() const {
     return path;
 }
 
 std::string TemporaryDirectory::writeToFile(const std::string &fileName, const std::string &content) {
-    const std::string &filePath = (path / fileName).string();
+    const std::string &filePath = getFilePath(fileName);
     std::ofstream o(filePath);
     o << content;
     o.close();
     return filePath;
+}
+
+const std::string TemporaryDirectory::getFilePath(const std::string &fileName) const {
+    return (path / fileName).string();
 }
 
 } // crayg
--- src/crayg/tests/fixtures/TemporaryDirectory.h
+++ src/crayg/tests/fixtures/TemporaryDirectory.h
@@ -8,7 +8,8 @@ namespace crayg {
 class TemporaryDirectory {
   public:
     TemporaryDirectory();
-    const boost::filesystem::path &getPath() const;
+    const boost::filesystem::path getPath() const;
+    const std::string getFilePath(const std::string &fileName) const;
     std::string writeToFile(const std::string &fileName, const std::string &content);
     virtual ~TemporaryDirectory();
 
