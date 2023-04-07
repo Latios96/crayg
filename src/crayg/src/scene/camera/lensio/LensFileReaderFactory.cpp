6185df3c8d2e2c8c0b9d4301d7418e36d7968fb0
Jan Honsbrok <jan.honsbrok@gmail.com>
2023-04-07 13:16:54 +0200

    feat: add JSON reader to reader factory

--- src/crayg/src/scene/camera/lensio/LensFileReaderFactory.cpp
+++ src/crayg/src/scene/camera/lensio/LensFileReaderFactory.cpp
@@ -1,14 +1,33 @@
 #include "LensFileReaderFactory.h"
+#include "LensFileJsonReader.h"
 #include "LensFileTextFileReader.h"
+#include "Logger.h"
 
 namespace crayg {
 
 std::unique_ptr<LensFileReader> LensFileReaderFactory::createLensFileReader(const std::string &filePath) {
-    return std::make_unique<LensFileTextFileReader>();
+    boost::filesystem::path path(filePath);
+    std::string extension = path.extension().string();
+
+    if (extension == ".txt") {
+        return std::make_unique<LensFileTextFileReader>();
+    } else if (extension == ".json") {
+        return std::make_unique<LensFileJsonReader>();
+    } else {
+        Logger::error("No LensFileReader found for extension {}", extension);
+        throw std::runtime_error(fmt::format("No LensFileReader found for extension {}", extension));
+    }
 }
 
 std::unique_ptr<LensFileReader> LensFileReaderFactory::createLensFileReader(LensFileFormat lensFileFormat) {
-    return std::make_unique<LensFileTextFileReader>();
+    if (lensFileFormat == LensFileFormat::TXT) {
+        return std::unique_ptr<LensFileTextFileReader>();
+    } else if (lensFileFormat == LensFileFormat::JSON) {
+        return std::unique_ptr<LensFileJsonReader>();
+    } else {
+        Logger::error("No LensFileReader found for extension {}", lensFileFormat);
+        throw std::runtime_error(fmt::format("No LensFileReader found for extension {}", lensFileFormat));
+    }
 }
 
 } // crayg
