bef7c937b72529a3d7744feed43df08d71d2f9af
Jan Honsbrok <jan.honsbrok@gmail.com>
2023-04-20 15:18:28 +0200

    feat: seed random number generator per thread

--- src/crayg/src/sampling/Random.cpp
+++ src/crayg/src/sampling/Random.cpp
@@ -1,8 +1,11 @@
 #include "Random.h"
-#include <cstdlib>
+#include <random>
+#include <thread>
 
 namespace crayg {
 float Random::random() {
-    return ((float)rand() / (RAND_MAX));
+    static thread_local std::uniform_real_distribution<float> distribution(0.0, 1.0);
+    static thread_local std::mt19937 generator(std::hash<std::thread::id>()(std::this_thread::get_id()));
+    return distribution(generator);
 }
 } // crayg
