a9df025ec7b1e9acf3e0e01955a503820aa84259
Jan Honsbrok <jan.honsbrok@gmail.com>
2023-06-12 22:33:47 +0200

    feat: register crayg::BaseTaskReporter::Task as Qt Metatype

--- src/standalonegui/widgets/QtMetaTypes.h
+++ src/standalonegui/widgets/QtMetaTypes.h
@@ -1,6 +1,7 @@
 #ifndef CRAYG_SRC_STANDALONEGUI_WIDGETS_QTMETATYPES_H_
 #define CRAYG_SRC_STANDALONEGUI_WIDGETS_QTMETATYPES_H_
 
+#include "utils/TaskReporter.h"
 #include <QMetaType>
 #include <image/ImageMetadata.h>
 #include <image/ImageSpec.h>
@@ -10,6 +11,7 @@ Q_DECLARE_METATYPE(crayg::ImageBucket);
 Q_DECLARE_METATYPE(crayg::BucketImageBuffer);
 Q_DECLARE_METATYPE(std::shared_ptr<crayg::BucketImageBuffer>);
 Q_DECLARE_METATYPE(crayg::ImageSpec);
+Q_DECLARE_METATYPE(crayg::BaseTaskReporter::Task);
 
 void registerQMetaTypes() {
     qRegisterMetaType<crayg::ImageMetadata>("ImageMetadata");
@@ -17,6 +19,7 @@ void registerQMetaTypes() {
     qRegisterMetaType<crayg::BucketImageBuffer>();
     qRegisterMetaType<std::shared_ptr<crayg::BucketImageBuffer>>("std::shared_ptr<BucketImageBuffer>");
     qRegisterMetaType<crayg::ImageSpec>("ImageSpec");
+    qRegisterMetaType<crayg::BaseTaskReporter::Task>("BaseTaskReporter::Task");
 }
 
 #endif // CRAYG_SRC_STANDALONEGUI_WIDGETS_QTMETATYPES_H_
