3b9600cc61764bd49bc958e9d35ea2bed6bd833a
Jan Honsbrok <jan.honsbrok@gmail.com>
2023-06-22 16:42:28 +0200

    feat: add first PointInstancer outline

---
+++ src/crayg/src/scene/primitives/PointInstancer.cpp
@@ -0,0 +1,9 @@
+#include "PointInstancer.h"
+
+namespace crayg {
+
+std::string PointInstancer::getType() {
+    return "PointInstancer";
+}
+
+} // crayg
---
+++ src/crayg/src/scene/primitives/PointInstancer.h
@@ -0,0 +1,29 @@
+#ifndef CRAYG_SRC_CRAYG_SRC_SCENE_PRIMITIVES_POINTINSTANCER_H_
+#define CRAYG_SRC_CRAYG_SRC_SCENE_PRIMITIVES_POINTINSTANCER_H_
+
+#include "basics/Transform.h"
+#include "scene/Imageable.h"
+#include "scene/SceneObject.h"
+#include <vector>
+
+namespace crayg {
+
+class PointInstancer : public SceneObject {
+  public:
+    std::string getType() override;
+    virtual ~PointInstancer() = default;
+
+    std::vector<int> ids;
+    std::vector<Transform> transforms;
+    std::vector<int> protoIndices;
+
+    struct Prototype {
+        std::vector<std::unique_ptr<Imageable>> members;
+    };
+
+    std::vector<std::unique_ptr<Prototype>> protos;
+};
+
+} // crayg
+
+#endif // CRAYG_SRC_CRAYG_SRC_SCENE_PRIMITIVES_POINTINSTANCER_H_
