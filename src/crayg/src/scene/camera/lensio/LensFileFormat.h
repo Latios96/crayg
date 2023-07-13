6e7f6a3fe02d89e1c6f86c00025af6b176008582
Jan Honsbrok <jan.honsbrok@gmail.com>
2023-07-13 16:00:13 +0200

    refactor: use CRAYG_FMT_ENUM_FORMATTER enum

--- src/crayg/src/scene/camera/lensio/LensFileFormat.h
+++ src/crayg/src/scene/camera/lensio/LensFileFormat.h
@@ -1,8 +1,7 @@
 #ifndef CRAYG_SRC_CRAYG_SRC_SCENE_CAMERA_LENSIO_LENSFILEFORMAT_H_
 #define CRAYG_SRC_CRAYG_SRC_SCENE_CAMERA_LENSIO_LENSFILEFORMAT_H_
 
-#include <fmt/core.h>
-#include <magic_enum.hpp>
+#include "utils/EnumUtils.h"
 
 namespace crayg {
 
@@ -10,14 +9,6 @@ enum class LensFileFormat { TXT, JSON };
 
 }
 
-template <> struct fmt::formatter<crayg::LensFileFormat> {
-    template <typename ParseContext> constexpr auto parse(ParseContext &ctx) {
-        return ctx.begin();
-    }
-
-    template <typename FormatContext> auto format(crayg::LensFileFormat const &lensFileFormat, FormatContext &ctx) {
-        return fmt::format_to(ctx.out(), magic_enum::enum_name(lensFileFormat));
-    };
-};
+CRAYG_FMT_ENUM_FORMATTER(crayg::LensFileFormat);
 
 #endif // CRAYG_SRC_CRAYG_SRC_SCENE_CAMERA_LENSIO_LENSFILEFORMAT_H_
