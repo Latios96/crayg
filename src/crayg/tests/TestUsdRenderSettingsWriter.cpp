d878bb5bee47b881314d0c2cdfde6604a41498a5
Jan Honsbrok <jan.honsbrok@gmail.com>
2023-08-01 13:05:58 +0200

    refactor: make const what can be const

--- src/crayg/tests/TestUsdRenderSettingsWriter.cpp
+++ src/crayg/tests/TestUsdRenderSettingsWriter.cpp
@@ -10,32 +10,32 @@ TEST_CASE("UsdRenderSettingsWriter::write") {
     auto stage = pxr::UsdStage::CreateInMemory();
 
     SECTION("should write rendersettings correctly") {
-        RenderSettings renderSettings(Resolution(1280, 720), 4, IntegratorType::RAYTRACING,
-                                      IntegratorSettings({{"AMBIENT_OCCLUSION:sampleCount", {8}}}),
-                                      IntersectorType::EMBREE, BucketSequenceType::LINE_BY_LINE,
-                                      BucketSamplerType::ADAPTIVE, 0.007f, 8, true);
+        const RenderSettings renderSettings(Resolution(1280, 720), 4, IntegratorType::RAYTRACING,
+                                            IntegratorSettings({{"AMBIENT_OCCLUSION:sampleCount", {8}}}),
+                                            IntersectorType::EMBREE, BucketSequenceType::LINE_BY_LINE,
+                                            BucketSamplerType::ADAPTIVE, 0.007f, 8, true);
 
         UsdRenderSettingsWriter usdRenderSettingsWriter(renderSettings);
         usdRenderSettingsWriter.write(stage);
 
-        auto usdRenderSettings = pxr::UsdRenderSettings(stage->GetPrimAtPath(pxr::SdfPath("/Render/settings")));
+        const auto usdRenderSettings = pxr::UsdRenderSettings(stage->GetPrimAtPath(pxr::SdfPath("/Render/settings")));
         const pxr::GfVec2i resolution =
             UsdUtils::getStaticAttributeValueAs<pxr::GfVec2i>(usdRenderSettings.GetResolutionAttr());
-        int maxSamples = UsdUtils::getStaticAttributeValueAs<int>(
+        const int maxSamples = UsdUtils::getStaticAttributeValueAs<int>(
             usdRenderSettings.GetPrim().GetAttribute(pxr::TfToken("maxSamples")));
-        pxr::TfToken integratorType = UsdUtils::getStaticAttributeValueAs<pxr::TfToken>(
+        const pxr::TfToken integratorType = UsdUtils::getStaticAttributeValueAs<pxr::TfToken>(
             usdRenderSettings.GetPrim().GetAttribute(pxr::TfToken("integratorType")));
         const int sampleCount = UsdUtils::getStaticAttributeValueAs<int>(
             usdRenderSettings.GetPrim().GetAttribute(pxr::TfToken("AMBIENT_OCCLUSION:sampleCount")));
-        pxr::TfToken intersectorType = UsdUtils::getStaticAttributeValueAs<pxr::TfToken>(
+        const pxr::TfToken intersectorType = UsdUtils::getStaticAttributeValueAs<pxr::TfToken>(
             usdRenderSettings.GetPrim().GetAttribute(pxr::TfToken("intersectorType")));
-        pxr::TfToken bucketSequenceType = UsdUtils::getStaticAttributeValueAs<pxr::TfToken>(
+        const pxr::TfToken bucketSequenceType = UsdUtils::getStaticAttributeValueAs<pxr::TfToken>(
             usdRenderSettings.GetPrim().GetAttribute(pxr::TfToken("bucketSequenceType")));
-        pxr::TfToken bucketSamplerType = UsdUtils::getStaticAttributeValueAs<pxr::TfToken>(
+        const pxr::TfToken bucketSamplerType = UsdUtils::getStaticAttributeValueAs<pxr::TfToken>(
             usdRenderSettings.GetPrim().GetAttribute(pxr::TfToken("bucketSamplerType")));
-        float adaptiveMaxError = UsdUtils::getStaticAttributeValueAs<float>(
+        const float adaptiveMaxError = UsdUtils::getStaticAttributeValueAs<float>(
             usdRenderSettings.GetPrim().GetAttribute(pxr::TfToken("adaptiveMaxError")));
-        int samplesPerAdaptivePass = UsdUtils::getStaticAttributeValueAs<int>(
+        const int samplesPerAdaptivePass = UsdUtils::getStaticAttributeValueAs<int>(
             usdRenderSettings.GetPrim().GetAttribute(pxr::TfToken("samplesPerAdaptivePass")));
         const int useSpectralLensing = UsdUtils::getStaticAttributeValueAs<int>(
             usdRenderSettings.GetPrim().GetAttribute(pxr::TfToken("useSpectralLensing")));
