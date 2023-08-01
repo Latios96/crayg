6b28ee530ba7cc2f7e8df8e421ef5da7269c9f87
Jan Honsbrok <jan.honsbrok@gmail.com>
2023-08-01 11:29:36 +0200

    feat: add RGB wavelength constants

--- src/crayg/src/scene/camera/realistic/Wavelengths.h
+++ src/crayg/src/scene/camera/realistic/Wavelengths.h
@@ -4,6 +4,13 @@
 
 namespace crayg {
 
+class WavelengthsRgb {
+  public:
+    static const inline float R = 700.f;
+    static const inline float G = 546.1;
+    static const inline float B = 435.8;
+};
+
 CRAYG_DTO_3(FraunhoferLine, std::string, symbol, std::string, elementName, float, wavelength);
 
 class FraunhoferLines {
