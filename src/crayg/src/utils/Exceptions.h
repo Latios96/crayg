72ac92e656d565292ebec98300740acde9c9932a
Jan Honsbrok <jan.honsbrok@gmail.com>
2023-09-27 19:40:23 +0200

    feat: add new macro for logging and throwing an exception

--- src/crayg/src/utils/Exceptions.h
+++ src/crayg/src/utils/Exceptions.h
@@ -1,8 +1,10 @@
 #ifndef CRAYG_SRC_CRAYG_SRC_UTILS_EXCEPTIONS_H_
 #define CRAYG_SRC_CRAYG_SRC_UTILS_EXCEPTIONS_H_
 
-#define CRAYG_LOG_AND_THROW_MESSAGE(message)                                                                           \
-    Logger::error("Error at {}:{}: {}", __FILE__, __LINE__, message);                                                  \
-    throw std::runtime_error(fmt::format("Error at {}:{}: {}", __FILE__, __LINE__, message));
+#define CRAYG_LOG_AND_THROW(exception)                                                                                 \
+    Logger::error("Error at {}:{}: {}", __FILE__, __LINE__, (exception.what()));                                       \
+    throw(exception);
+
+#define CRAYG_LOG_AND_THROW_MESSAGE(message) CRAYG_LOG_AND_THROW(std::runtime_error(message))
 
 #endif // CRAYG_SRC_CRAYG_SRC_UTILS_EXCEPTIONS_H_
