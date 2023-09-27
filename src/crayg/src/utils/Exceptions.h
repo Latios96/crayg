656bee0f0e74aee1e8b2bdbe419509eaaa113265
Jan Honsbrok <jan.honsbrok@gmail.com>
2023-09-27 20:25:36 +0200

    feat: include stack trace when trowing an exception

--- src/crayg/src/utils/Exceptions.h
+++ src/crayg/src/utils/Exceptions.h
@@ -1,9 +1,11 @@
 #ifndef CRAYG_SRC_CRAYG_SRC_UTILS_EXCEPTIONS_H_
 #define CRAYG_SRC_CRAYG_SRC_UTILS_EXCEPTIONS_H_
 #include "Logger.h"
+#include <cpptrace/cpptrace.hpp>
 
 #define CRAYG_LOG_AND_THROW(exception)                                                                                 \
-    Logger::error("Error at {}:{}: {}", __FILE__, __LINE__, (exception.what()));                                       \
+    const auto raw_trace = cpptrace::generate_raw_trace();                                                             \
+    Logger::error("Error at {}:{}: {}\n{}", __FILE__, __LINE__, (exception.what()), raw_trace.resolve().to_string());  \
     throw(exception);
 
 #define CRAYG_LOG_AND_THROW_MESSAGE(message) CRAYG_LOG_AND_THROW(std::runtime_error(message))
