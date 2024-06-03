aafe9aa154e07c88effe95dd55be99ce2482dce7
Jan Honsbrok <jan.honsbrok@gmail.com>
2024-06-03 17:07:12 +0200

    fix: make sure tests can run when tracing is enabled

--- src/crayg/tests/main.cpp
+++ src/crayg/tests/main.cpp
@@ -1,8 +1,25 @@
 #define CATCH_CONFIG_RUNNER
 
+#include "fixtures/TemporaryDirectory.h"
+#include "utils/tracing/CraygTracing.h"
 #include <catch2/catch.hpp>
+#include <fmt/format.h>
 
 int main(int argc, char *argv[]) {
+    crayg::TemporaryDirectory temporaryDirectory;
+    CRG_IF_TRACE({
+        std::string traceFilePath = temporaryDirectory.getFilePath("trace.json");
+        std::cout << fmt::format("Tracing enabled, tracing to {}", traceFilePath) << std::endl;
+        mtr_init(traceFilePath.c_str());
+    });
+
     int result = Catch::Session().run(argc, argv);
+
+    CRG_IF_TRACE({
+        mtr_flush();
+        std::cout << "Shutting down trace." << std::endl;
+        mtr_shutdown();
+        std::cout << "Flushing trace." << std::endl;
+    });
     return result;
 }
