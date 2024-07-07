b2324eb2f2fdfe33f8aaa2a74c6d2f9f5f5defc0
Jan Honsbrok <jan.honsbrok@gmail.com>
2024-07-07 13:09:51 +0200

    feat: add formatter for QString

--- src/standalonegui/QtBase.h
+++ src/standalonegui/QtBase.h
@@ -2,7 +2,18 @@
 
 #include <QPoint>
 #include <QSize>
+#include <QString>
 #include <fmt/format.h>
+
+template <> struct fmt::formatter<QString> {
+    template <typename ParseContext> constexpr auto parse(ParseContext &ctx) {
+        return ctx.begin();
+    }
+
+    template <typename FormatContext> auto format(QString const &qString, FormatContext &ctx) {
+        return fmt::format_to(ctx.out(), "{}", qString.toStdString());
+    };
+};
 
 template <> struct fmt::formatter<QPoint> {
     template <typename ParseContext> constexpr auto parse(ParseContext &ctx) {
