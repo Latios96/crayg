6e01eeb124a287cc2b370e4170110d60c02d208f
Jan Honsbrok <jan.honsbrok@gmail.com>
2024-10-16 20:13:01 +0200

    feat: use Schlick approximation for reflectance

--- src/crayg/src/scene/shadingnetworks/materials/UsdPreviewSurface.cpp
+++ src/crayg/src/scene/shadingnetworks/materials/UsdPreviewSurface.cpp
@@ -1,5 +1,6 @@
 #include "UsdPreviewSurface.h"
 #include "Logger.h"
+#include "basics/MathUtils.h"
 #include "sampling/Sampling.h"
 #include "scene/Imageable.h"
 
@@ -33,13 +34,28 @@ Color UsdPreviewSurface::getReflectivity(const SurfaceInteraction &surfaceIntera
     return Color::createGrey(metallic.evaluate(surfaceInteraction));
 }
 
+float F_schlick(float ior, const Vector3f &view, const Vector3f &halfVector) {
+    const float iorDiff = 1 - ior;
+    const float iorSum = 1 + ior;
+    const float F_0 = (iorDiff * iorDiff) / (iorSum * iorSum);
+    const float oneMinus = 1.f - halfVector.dot(view);
+    return F_0 + (1.f - F_0) * oneMinus * oneMinus * oneMinus * oneMinus * oneMinus;
+}
+
 void UsdPreviewSurface::getLobes(const SurfaceInteraction &surfaceInteraction, Lobes &lobes) {
     lobes.specular.weight = getReflectivity(surfaceInteraction);
+    const Vector3f halfVector =
+        (lobes.specular.sampleDirection.direction + surfaceInteraction.ray.direction).normalize();
+
+    float reflectance = 0;
     if (!lobes.specular.weight.isBlack()) {
         lobes.specular.sampleDirection = surfaceInteraction.spawnReflectionRayFromSurface();
+        const float evaluatedIor = ior.evaluate(surfaceInteraction);
+        reflectance = F_schlick(evaluatedIor, surfaceInteraction.ray.direction, halfVector);
     }
 
-    lobes.diffuse.weight = (Color::createWhite() - lobes.specular.weight) * diffuseColor.evaluate(surfaceInteraction);
+    lobes.diffuse.weight =
+        (Color::createWhite() - lobes.specular.weight * reflectance) * diffuseColor.evaluate(surfaceInteraction);
     if (!lobes.diffuse.weight.isBlack()) {
         const Vector3f randomDirOnHemisphere = Sampling::uniformSampleHemisphere();
         auto orthonormalBasis = surfaceInteraction.getOrthonormalBasis();
