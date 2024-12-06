30614b7419d9e7d22ae66c11cdf5624a0d2b4c3c
Jan Honsbrok <jan.honsbrok@gmail.com>
2024-12-06 16:22:42 +0100

    refactor: extract method

--- src/crayg/src/intersectors/embree/EmbreeBvhBuilder.cpp
+++ src/crayg/src/intersectors/embree/EmbreeBvhBuilder.cpp
@@ -46,6 +46,26 @@ RTCScene buildFromSceneObjects(RTCDevice device, const std::vector<std::shared_p
                                std::vector<EmbreeProtoInstanceMappingEntry> *globalProtoGeomToSceneObject,
                                EmbreeInstanceIdToInstanceInfo *embreeInstanceIdToInstanceInfo);
 
+void convertMatrixToEmbree(float *transform, const Matrix4x4f &matrix) {
+    // format: RTC_FORMAT_FLOAT4X4_COLUMN_MAJOR
+    transform[0] = matrix.values(0, 0);
+    transform[1] = matrix.values(1, 0);
+    transform[2] = matrix.values(2, 0);
+    transform[3] = matrix.values(3, 0);
+    transform[4] = matrix.values(0, 1);
+    transform[5] = matrix.values(1, 1);
+    transform[6] = matrix.values(2, 1);
+    transform[7] = matrix.values(3, 1);
+    transform[8] = matrix.values(0, 2);
+    transform[9] = matrix.values(1, 2);
+    transform[10] = matrix.values(2, 2);
+    transform[11] = matrix.values(3, 2);
+    transform[12] = matrix.values(0, 3);
+    transform[13] = matrix.values(1, 3);
+    transform[14] = matrix.values(2, 3);
+    transform[15] = matrix.values(3, 3);
+}
+
 void addPointInstancer(RTCDevice device, RTCScene rtcScene, std::shared_ptr<PointInstancer> &pointInstancer,
                        unsigned int pointInstancerIndex,
                        std::vector<EmbreeProtoInstanceMappingEntry> *globalProtoGeomToSceneObject,
@@ -71,24 +91,10 @@ void addPointInstancer(RTCDevice device, RTCScene rtcScene, std::shared_ptr<Poin
         auto protoId = pointInstancer->protoIndices[instance];
         RTCGeometry instancedProto = rtcNewGeometry(device, RTC_GEOMETRY_TYPE_INSTANCE);
         rtcSetGeometryInstancedScene(instancedProto, protoIdToRtcScene[protoId]);
-        float transform[16];
+
         auto &matrix = (pointInstancer->transforms[instance]).matrix; // todo rework this
-        transform[0] = matrix.values(0, 0);
-        transform[1] = matrix.values(1, 0);
-        transform[2] = matrix.values(2, 0);
-        transform[3] = matrix.values(3, 0);
-        transform[4] = matrix.values(0, 1);
-        transform[5] = matrix.values(1, 1);
-        transform[6] = matrix.values(2, 1);
-        transform[7] = matrix.values(3, 1);
-        transform[8] = matrix.values(0, 2);
-        transform[9] = matrix.values(1, 2);
-        transform[10] = matrix.values(2, 2);
-        transform[11] = matrix.values(3, 2);
-        transform[12] = matrix.values(0, 3);
-        transform[13] = matrix.values(1, 3);
-        transform[14] = matrix.values(2, 3);
-        transform[15] = matrix.values(3, 3);
+        float transform[16];
+        convertMatrixToEmbree(transform, matrix);
 
         rtcSetGeometryTransform(instancedProto, 0, RTC_FORMAT_FLOAT4X4_COLUMN_MAJOR, transform);
         rtcCommitGeometry(instancedProto);
