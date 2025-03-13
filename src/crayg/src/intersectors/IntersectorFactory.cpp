d911cca2658be7265810d94aff3edcca5970dd64
Jan Honsbrok <jan.honsbrok@gmail.com>
2025-03-13 10:09:38 +0100

    refactor: split factory implementation into own file

---
+++ src/crayg/src/intersectors/IntersectorFactory.cpp
@@ -0,0 +1,24 @@
+#include "IntersectorFactory.h"
+#include "intersectors/embree/EmbreeBvhBuilder.h"
+#include "intersectors/embree/EmbreeSceneIntersector.h"
+#include "utils/Exceptions.h"
+
+namespace crayg {
+
+std::shared_ptr<SceneIntersector> IntersectorFactory::createSceneIntersector(const IntersectorType &intersectorType,
+                                                                             Scene &scene) {
+    switch (intersectorType) {
+    case IntersectorType::EMBREE:
+        return createEmbreeBvh(scene);
+    default:
+        CRAYG_LOG_AND_THROW(std::runtime_error(fmt::format(R"(Unsupported Intersector type: "{}")", intersectorType)));
+    }
+}
+
+std::shared_ptr<SceneIntersector> IntersectorFactory::createEmbreeBvh(Scene &scene) {
+    EmbreeBvhBuilder embreeBvhBuilder(scene);
+    auto bvh = embreeBvhBuilder.build();
+    return std::make_shared<EmbreeSceneIntersector>(scene, std::move(bvh));
+}
+
+}
--- src/crayg/src/intersectors/IntersectorFactory.h
+++ src/crayg/src/intersectors/IntersectorFactory.h
@@ -1,30 +1,16 @@
 #pragma once
 
+#include "IntersectorType.h"
 #include "SceneIntersector.h"
-#include "intersectors/embree/EmbreeBvhBuilder.h"
-#include "intersectors/embree/EmbreeSceneIntersector.h"
-#include "utils/Exceptions.h"
 
 namespace crayg {
 
 class IntersectorFactory {
   public:
     static std::shared_ptr<SceneIntersector> createSceneIntersector(const IntersectorType &intersectorType,
-                                                                    Scene &scene) {
-        switch (intersectorType) {
-        case IntersectorType::EMBREE:
-            return createEmbreeBvh(scene);
-        default:
-            CRAYG_LOG_AND_THROW(
-                std::runtime_error(fmt::format(R"(Unsupported Intersector type: "{}")", intersectorType)));
-        }
-    }
+                                                                    Scene &scene);
 
-    static std::shared_ptr<SceneIntersector> createEmbreeBvh(Scene &scene) {
-        EmbreeBvhBuilder embreeBvhBuilder(scene);
-        auto bvh = embreeBvhBuilder.build();
-        return std::make_shared<EmbreeSceneIntersector>(scene, std::move(bvh));
-    }
+    static std::shared_ptr<SceneIntersector> createEmbreeBvh(Scene &scene);
 };
 
 } // crayg
