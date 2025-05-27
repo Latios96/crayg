a3eb52c696649b2b5cb4b81d6eacab30e625721e
Jan Honsbrok <jan.honsbrok@gmail.com>
2025-05-27 16:21:09 +0200

    feat: add AtomicFloats

---
+++ src/crayg/src/image/newbufferapi/AtomicFloats.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "BitCast.h"
+#include <atomic>
+
+namespace crayg {
+
+template <typename FloatingPointType, typename StorageType> struct AtomicFloatingPoint {
+
+    FloatingPointType get() const {
+        return BitCast::floatingFromBits<FloatingPointType, StorageType>(bits);
+    }
+
+    void atomicAdd(FloatingPointType x) {
+        StorageType old_bits = bits.load();
+        StorageType new_bits;
+        do {
+            double old_val = BitCast::floatingFromBits<FloatingPointType, StorageType>(old_bits);
+            double new_val = old_val + x;
+            new_bits = BitCast::floatingToBits<FloatingPointType, StorageType>(new_val);
+        } while (!bits.compare_exchange_weak(old_bits, new_bits));
+    }
+
+  private:
+    std::atomic<StorageType> bits;
+};
+
+typedef AtomicFloatingPoint<float, uint32_t> AtomicFloat;
+typedef AtomicFloatingPoint<double, uint64_t> AtomicDouble;
+
+}
---
+++ src/crayg/tests/newbufferapi/TestAtomicFloats.cpp
@@ -0,0 +1,58 @@
+#include <catch2/catch.hpp>
+#include <image/newbufferapi/AtomicFloats.h>
+#include <tbb/parallel_for.h>
+
+namespace crayg {
+
+TEST_CASE("AtomicFloats::construct") {
+    SECTION("float") {
+        AtomicFloat atomicFloat;
+        REQUIRE(atomicFloat.get() == 0);
+    }
+
+    SECTION("double") {
+        AtomicDouble atomicDouble;
+        REQUIRE(atomicDouble.get() == 0);
+    }
+}
+
+TEST_CASE("AtomicFloats::addAtomic") {
+
+    SECTION("should add correctly for float serial") {
+        AtomicFloat atomicFloat;
+
+        atomicFloat.atomicAdd(42);
+        REQUIRE(atomicFloat.get() == 42);
+
+        atomicFloat.atomicAdd(42);
+        REQUIRE(atomicFloat.get() == 84);
+    }
+
+    SECTION("should add correctly for float parallel") {
+        AtomicFloat atomicFloat;
+
+        tbb::parallel_for(0, 32000, [&atomicFloat](int i) { atomicFloat.atomicAdd(1); });
+
+        REQUIRE(atomicFloat.get() == 32000);
+    }
+
+    SECTION("should add correctly for double serial") {
+        AtomicDouble atomicDouble;
+
+        atomicDouble.atomicAdd(42);
+        REQUIRE(atomicDouble.get() == 42);
+
+        atomicDouble.atomicAdd(42);
+        REQUIRE(atomicDouble.get() == 84);
+    }
+
+    SECTION("should add correctly for double parallel") {
+        AtomicDouble atomicDouble;
+
+        tbb::parallel_for(0, 32000, [&atomicDouble](int i) { atomicDouble.atomicAdd(1); });
+
+        REQUIRE(atomicDouble.get() == 32000);
+    }
+}
+
+}
