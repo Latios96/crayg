b0a17bbbf50270e6c257c71ae7c244a6a3a1b680
Jan Honsbrok <jan.honsbrok@gmail.com>
2025-06-03 21:49:10 +0200

    fix: make formatters in macros work
    Huge issues in MSVC because of ODR rule...

--- src/crayg/src/utils/EnumFormatter.h
+++ src/crayg/src/utils/EnumFormatter.h
@@ -1,12 +1,15 @@
 #pragma once
 #include <fmt/format.h>
+#include <fmt/ostream.h>
 #include <magic_enum.hpp>
 
 #define CRAYG_FMT_ENUM_FORMATTER(FullQualifiedType)                                                                    \
-    template <> struct fmt::formatter<FullQualifiedType> : formatter<string_view> {                                    \
+    namespace crayg {                                                                                                  \
                                                                                                                        \
-        auto format(const FullQualifiedType &enumValue, format_context &ctx) const -> format_context::iterator {       \
-            const std::string_view name = magic_enum::enum_name(enumValue);                                            \
-            return formatter<string_view>::format(name, ctx);                                                          \
-        }                                                                                                              \
-    };
+    inline std::ostream &operator<<(std::ostream &os, const FullQualifiedType &v) {                                    \
+        os << magic_enum::enum_name(v);                                                                                \
+        return os;                                                                                                     \
+    }                                                                                                                  \
+    }                                                                                                                  \
+                                                                                                                       \
+    template <> struct fmt::formatter<FullQualifiedType> : ostream_formatter {};
