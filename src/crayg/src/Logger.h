83e849ea6124c2207f63b365ed5edbd3d2c89b7e
Jan Honsbrok <jan.honsbrok@gmail.com>
2025-06-03 21:46:35 +0200

    fix: make logger compatible

--- src/crayg/src/Logger.h
+++ src/crayg/src/Logger.h
@@ -12,17 +12,29 @@ class Logger {
     enum class LevelEnum { trace = 0, debug = 1, info = 2, warn = 3, err = 4, critical = 5, off = 6 };
     static void setLevel(LevelEnum level);
 
-    template <typename... Args> static void trace(const char *fmt, const Args &...args);
+    template <typename... Args> inline static void trace(spdlog::format_string_t<Args...> fmt, Args &&...args) {
+        spdlog::default_logger_raw()->trace(fmt, std::forward<Args>(args)...);
+    }
 
-    template <typename... Args> static void debug(const char *fmt, const Args &...args);
+    template <typename... Args> inline static void debug(spdlog::format_string_t<Args...> fmt, Args &&...args) {
+        spdlog::default_logger_raw()->debug(fmt, std::forward<Args>(args)...);
+    }
 
-    template <typename... Args> static void info(const char *fmt, const Args &...args);
+    template <typename... Args> inline static void info(spdlog::format_string_t<Args...> fmt, Args &&...args) {
+        spdlog::default_logger_raw()->info(fmt, std::forward<Args>(args)...);
+    }
 
-    template <typename... Args> static void warning(const char *fmt, const Args &...args);
+    template <typename... Args> inline static void warning(spdlog::format_string_t<Args...> fmt, Args &&...args) {
+        spdlog::default_logger_raw()->warn(fmt, std::forward<Args>(args)...);
+    }
 
-    template <typename... Args> static void error(const char *fmt, const Args &...args);
+    template <typename... Args> inline static void error(spdlog::format_string_t<Args...> fmt, Args &&...args) {
+        spdlog::default_logger_raw()->error(fmt, std::forward<Args>(args)...);
+    }
 
-    template <typename... Args> static void critical(const char *fmt, const Args &...args);
+    template <typename... Args> inline static void critical(spdlog::format_string_t<Args...> fmt, Args &&...args) {
+        spdlog::default_logger_raw()->critical(fmt, std::forward<Args>(args)...);
+    }
 
     template <typename T> static void trace(const T &msg);
 
@@ -38,30 +50,6 @@ class Logger {
     static void logToFile(const std::string &fileName);
     static void backtrace();
 };
-
-template <typename... Args> void Logger::trace(const char *fmt, const Args &...args) {
-    spdlog::trace(fmt, args...);
-}
-
-template <typename... Args> void Logger::debug(const char *fmt, const Args &...args) {
-    spdlog::debug(fmt, args...);
-}
-
-template <typename... Args> void Logger::info(const char *fmt, const Args &...args) {
-    spdlog::info(fmt, args...);
-}
-
-template <typename... Args> void Logger::warning(const char *fmt, const Args &...args) {
-    spdlog::warn(fmt, args...);
-}
-
-template <typename... Args> void Logger::error(const char *fmt, const Args &...args) {
-    spdlog::error(fmt, args...);
-}
-
-template <typename... Args> void Logger::critical(const char *fmt, const Args &...args) {
-    spdlog::critical(fmt, args...);
-}
 
 template <typename T> void Logger::trace(const T &msg) {
     spdlog::trace(msg);
