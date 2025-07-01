b97a244be88f49bad1f504b7c01f7429b9199136
Jan Honsbrok <jan.honsbrok@gmail.com>
2025-07-01 17:57:50 +0200

    fix: add missing tests

--- src/crayg/tests/film/buffers/TestFilmBufferVariants.cpp
+++ src/crayg/tests/film/buffers/TestFilmBufferVariants.cpp
@@ -2,6 +2,7 @@
 #include <catch2/catch.hpp>
 
 namespace crayg {
+
 TEST_CASE("FilmBufferVariants::getAsAccumulationBufferVariantPtr") {
 
     SECTION("should return accumulation buffer ptr") {
@@ -23,6 +24,31 @@ TEST_CASE("FilmBufferVariants::getAsAccumulationBufferVariantPtr") {
         const auto buffer = std::get<0>(testData);
 
         REQUIRE_FALSE(FilmBufferVariants::getAsAccumulationBufferVariantPtr(buffer).has_value());
+
+        FilmBufferVariants::freeFilmBufferVariantPtr(buffer);
+    }
+}
+
+TEST_CASE("FilmBufferVariants::getAsValueBufferVariantPtr") {
+
+    SECTION("should return accumulation buffer ptr") {
+        const auto &testData =
+            GENERATE(table<FilmBufferVariantPtr>({new FloatValueBuffer(10, 5), new Color3fValueBuffer(10, 5)}));
+
+        const auto buffer = std::get<0>(testData);
+
+        REQUIRE(FilmBufferVariants::getAsValueBufferVariantPtr(buffer).has_value());
+
+        FilmBufferVariants::freeFilmBufferVariantPtr(buffer);
+    }
+
+    SECTION("should return empty optional for non-accumulation buffer") {
+        const auto &testData = GENERATE(
+            table<FilmBufferVariantPtr>({new FloatAccumulationBuffer(10, 5), new Color3fAccumulationBuffer(10, 5)}));
+
+        const auto buffer = std::get<0>(testData);
+
+        REQUIRE_FALSE(FilmBufferVariants::getAsValueBufferVariantPtr(buffer).has_value());
 
         FilmBufferVariants::freeFilmBufferVariantPtr(buffer);
     }
