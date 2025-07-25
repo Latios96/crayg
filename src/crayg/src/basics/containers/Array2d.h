3f8da075a51182cfffa4cdc7a6b0a7aeb1aaff3b
Jan Honsbrok <jan.honsbrok@gmail.com>
2025-07-25 14:39:44 +0200

    feat: improve Array2d implementation

--- src/crayg/src/basics/containers/Array2d.h
+++ src/crayg/src/basics/containers/Array2d.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "basics/Vector2.h"
+#include "utils/Exceptions.h"
 #include "utils/Preconditions.h"
 #include <vector>
 
@@ -11,48 +13,81 @@ template <typename T> class Array2d {
     Array2d() = default;
 
     Array2d(size_t width, size_t height) : width(width), height(height) {
-        storage.resize(size());
+        storage = std::vector<T>(width * height);
     }
 
-    Array2d(Array2d<T> &&other) noexcept {
-        storage = std::move(other.storage);
-        height = other.height;
-        width = other.width;
+    Array2d(size_t width, size_t height, std::initializer_list<T> initializer) : width(width), height(height) {
+        CRAYG_CHECK_EQUALS(width * height, initializer.size());
+        storage = initializer;
     }
 
-    const T &get(size_t xIndex, size_t yIndex) const {
-        CRAYG_CHECKD_IS_VALID_INDEX(index(xIndex, yIndex), size());
-        return storage[index(xIndex, yIndex)];
+    Array2d(size_t width, size_t height, float *data) : width(width), height(height) {
+        storage = std::vector<T>(width * height);
+        for (size_t i = 0; i < storage.size(); i++) {
+            storage[i] = data[i];
+        }
     }
 
-    T &get(size_t xIndex, size_t yIndex) {
-        CRAYG_CHECKD_IS_VALID_INDEX(index(xIndex, yIndex), size());
-        return storage[index(xIndex, yIndex)];
+    T &operator[](const Vector2i &i) {
+        CRAYG_CHECKD_IS_VALID_INDEX(index(i.x, i.y), size());
+        return storage[index(i.x, i.y)];
     }
 
-    void set(size_t xIndex, size_t yIndex, const T &value) {
-        CRAYG_CHECKD_IS_VALID_INDEX(index(xIndex, yIndex), size());
-        storage[index(xIndex, yIndex)] = value;
+    const T &operator[](const Vector2i &i) const {
+        CRAYG_CHECKD_IS_VALID_INDEX(index(i.x, i.y), size());
+        return storage[index(i.x, i.y)];
     }
 
     size_t index(size_t xIndex, size_t yIndex) const {
         return xIndex + width * yIndex;
     }
 
+    friend bool operator==(const Array2d &lhs, const Array2d &rhs) {
+        return lhs.storage == rhs.storage && lhs.width == rhs.width && lhs.height == rhs.height;
+    }
+
+    friend bool operator!=(const Array2d &lhs, const Array2d &rhs) {
+        return !(lhs == rhs);
+    }
+
     T *data() {
         return storage.data();
     }
 
-    size_t size() {
+    size_t size() const {
         return width * height;
     }
 
-    T *begin() {
-        return storage.data();
+    auto begin() {
+        return storage.begin();
     }
 
-    T *end() {
-        return storage.data() + size();
+    auto end() {
+        return storage.end();
+    }
+
+    auto begin() const {
+        return storage.begin();
+    }
+
+    auto end() const {
+        return storage.end();
+    }
+
+    auto front() {
+        return storage.front();
+    }
+
+    auto back() {
+        return storage.back();
+    }
+
+    auto front() const {
+        return storage.front();
+    }
+
+    auto back() const {
+        return storage.back();
     }
 
   private:
--- src/crayg/tests/containers/TestArray2d.cpp
+++ src/crayg/tests/containers/TestArray2d.cpp
@@ -1,5 +1,6 @@
 #include "basics/containers/Array2d.h"
 #include <catch2/catch.hpp>
+#include <numeric>
 
 namespace crayg {
 
@@ -18,16 +19,33 @@ TEST_CASE("Array2d::construct") {
         REQUIRE(array2d.data() != nullptr);
         REQUIRE(array2d.data()[0] == 0);
     }
+
+    SECTION("should construct from initializer list") {
+        Array2d<float> array2d(3, 3, {0.5, 0.5, 0.5, 0.5, 1.0, 0.5, 0.5, 0.5, 0.5});
+
+        REQUIRE(array2d == Array2d<float>(3, 3, {0.5, 0.5, 0.5, 0.5, 1.0, 0.5, 0.5, 0.5, 0.5}));
+    }
+
+    SECTION("should construct from data ptr") {
+        std::vector<float> dataToConstructFrom({0.5, 0.5, 0.5, 0.5, 1.0, 0.5, 0.5, 0.5, 0.5});
+        Array2d<float> array2d(3, 3, dataToConstructFrom.data());
+
+        REQUIRE(array2d == Array2d<float>(3, 3, {0.5, 0.5, 0.5, 0.5, 1.0, 0.5, 0.5, 0.5, 0.5}));
+    }
+
+    SECTION("should fail if initializer list does not match width and height") {
+        REQUIRE_THROWS(Array2d<float>(3, 3, {0.5, 0.5, 0.5, 0.5, 1.0, 0.5, 0.5, 0.5}));
+    }
 }
 
 TEST_CASE("Array2d::get") {
     SECTION("should return desired element") {
         Array2d<int> array2d(2, 3);
 
-        array2d.get(0, 2) = 1;
-        array2d.get(1, 2) = 5;
-        REQUIRE(array2d.get(0, 2) == 1);
-        REQUIRE(array2d.get(1, 2) == 5);
+        array2d[{0, 2}] = 1;
+        array2d[{1, 2}] = 5;
+        REQUIRE(array2d[{0, 2}] == 1);
+        REQUIRE(array2d[{1, 2}] == 5);
     }
 }
 
@@ -35,23 +53,45 @@ TEST_CASE("Array2d::set") {
     SECTION("should return desired element") {
         Array2d<int> array2d(2, 3);
 
-        array2d.set(0, 2, 1);
-        array2d.set(1, 2, 5);
-        REQUIRE(array2d.get(0, 2) == 1);
-        REQUIRE(array2d.get(1, 2) == 5);
+        array2d[{0, 2}] = 1;
+        array2d[{1, 2}] = 5;
+        REQUIRE(array2d[{0, 2}] == 1);
+        REQUIRE(array2d[{1, 2}] == 5);
+    }
+}
+
+TEST_CASE("Array2d::comparison") {
+    SECTION("should be equal") {
+        Array2d<float> array2d(3, 3, {0.5, 0.5, 0.5, 0.5, 1.0, 0.5, 0.5, 0.5, 0.5});
+
+        REQUIRE(array2d == Array2d<float>(3, 3, {0.5, 0.5, 0.5, 0.5, 1.0, 0.5, 0.5, 0.5, 0.5}));
+    }
+
+    SECTION("should not be equal") {
+        Array2d<float> array2d(3, 3, {0.5, 0.5, 0.5, 0.5, 1.0, 0.5, 0.5, 0.5, 0.5});
+
+        REQUIRE_FALSE(array2d == Array2d<float>(3, 3, {0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5}));
     }
 }
 
 TEST_CASE("Array2d::iterate") {
-    SECTION("should visit all pixels") {
-        Array2d<int> array2d(2, 3);
+    SECTION("should compute sum of all values") {
+        Array2d<float> array2d(3, 3, {0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5});
 
-        int counter = 0;
-        for (auto pixelValue : array2d) {
-            counter++;
+        const float sum = std::accumulate(array2d.begin(), array2d.end(), 0.f);
+
+        REQUIRE(sum == 4.5f);
+    }
+
+    SECTION("should iterate over all values") {
+        Array2d<float> array2d(3, 3, {0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5});
+
+        float sum = 0;
+        for (auto &v : array2d) {
+            sum += v;
         }
 
-        REQUIRE(counter == 6);
+        REQUIRE(sum == 4.5f);
     }
 }
 
