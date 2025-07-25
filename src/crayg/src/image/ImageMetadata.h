010c44a8426b0b813f0905d915d30395f8c2b262
Jan Honsbrok <jan.honsbrok@gmail.com>
2025-07-25 15:24:38 +0200

    refactor: replace ImageMetadataValue typedef with own type

--- src/crayg/src/image/ImageMetadata.h
+++ src/crayg/src/image/ImageMetadata.h
@@ -11,7 +11,38 @@
 
 namespace crayg {
 
-typedef std::variant<std::string, int, float, std::chrono::seconds> ImageMetadataValue;
+typedef std::variant<std::string, int, float, std::chrono::seconds> _ImageMetadataValue;
+
+struct ImageMetadataValue : std::variant<std::string, int, float, std::chrono::seconds> {
+
+    ImageMetadataValue() = default;
+
+    ImageMetadataValue(const char *value) : _ImageMetadataValue(value) {
+    }
+
+    ImageMetadataValue(const std::string &value) : _ImageMetadataValue(value) {
+    }
+
+    ImageMetadataValue(int value) : _ImageMetadataValue(value) {
+    }
+
+    ImageMetadataValue(float value) : _ImageMetadataValue(value) {
+    }
+
+    ImageMetadataValue(std::chrono::seconds value) : _ImageMetadataValue(value) {
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const ImageMetadataValue &value) {
+        std::visit([&os](auto v) { os << fmt::format("{}", v); }, value);
+        return os;
+    }
+};
+
+}
+
+template <> struct fmt::formatter<crayg::ImageMetadataValue> : ostream_formatter {};
+
+namespace crayg {
 
 class ImageMetadataTokens {
   public:
