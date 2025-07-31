d228930a2edc278cae855a15e77cb6a5451fe7f4
Jan Honsbrok <jan.honsbrok@gmail.com>
2025-07-31 10:50:09 +0200

    refactor: extract ImageBufferTypeTrait into own file

--- src/crayg/src/image/io/imageformatwriters/png/PngImageFormatWriter.cpp
+++ src/crayg/src/image/io/imageformatwriters/png/PngImageFormatWriter.cpp
@@ -1,6 +1,7 @@
 #include "PngImageFormatWriter.h"
 
 #include "Logger.h"
+#include "image/io/imageformatwriters/ImageBufferTypeTrait.h"
 
 #include <lodepng.h>
 #include <tbb/parallel_for.h>
@@ -24,58 +25,6 @@ void appendImageMetadata(const ImageMetadata &imageMetadata, PngWriteData &write
         lodepng_add_text(&writeData.state.info_png, key.c_str(), value.c_str());
     }
 }
-
-template <typename ImageBufferType> struct ImageBufferTypeTrait {
-    static int getWidth(const ImageBufferType &bufferType);
-    static int getHeight(const ImageBufferType &bufferType);
-    static PixelFormat getPixelFormat(const ImageBufferType &bufferType);
-    static int getColorChannelCount(const ImageBufferType &bufferType);
-    static void *getDataPtr(const ImageBufferType &bufferType);
-};
-
-template <> struct ImageBufferTypeTrait<PixelBuffer> {
-    static int getWidth(const PixelBuffer &pixelBuffer) {
-        return pixelBuffer.getWidth();
-    }
-
-    static int getHeight(const PixelBuffer &pixelBuffer) {
-        return pixelBuffer.getHeight();
-    }
-
-    static PixelFormat getPixelFormat(const PixelBuffer &pixelBuffer) {
-        return pixelBuffer.getPixelFormat();
-    }
-
-    static int getColorChannelCount(const PixelBuffer &pixelBuffer) {
-        return pixelBuffer.getColorChannelCount();
-    }
-
-    static void *getDataPtr(const PixelBuffer &pixelBuffer) {
-        return std::visit([](auto ptr) { return (void *)ptr; }, pixelBuffer.getData());
-    }
-};
-
-template <> struct ImageBufferTypeTrait<FilmBufferVariantPtr> {
-    static int getWidth(const FilmBufferVariantPtr &filmBufferVariant) {
-        return FilmBufferVariants::getWidth(filmBufferVariant);
-    }
-
-    static int getHeight(const FilmBufferVariantPtr &filmBufferVariant) {
-        return FilmBufferVariants::getHeight(filmBufferVariant);
-    }
-
-    static PixelFormat getPixelFormat(const FilmBufferVariantPtr &filmBufferVariant) {
-        return FilmBufferVariants::getPixelFormat(filmBufferVariant);
-    }
-
-    static int getColorChannelCount(const FilmBufferVariantPtr &filmBufferVariant) {
-        return FilmBufferVariants::getChannelCount(filmBufferVariant);
-    }
-
-    static void *getDataPtr(const FilmBufferVariantPtr &filmBufferVariant) {
-        return FilmBufferVariants::getDataPtr(filmBufferVariant);
-    }
-};
 
 template <typename BufferType> void getWriteDataForChannel(PngWriteData &pngWriteData, const BufferType &buffer) {
     pngWriteData.width = ImageBufferTypeTrait<BufferType>::getWidth(buffer);
