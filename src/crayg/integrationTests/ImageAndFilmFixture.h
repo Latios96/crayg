6847c8d2a96b9dd773458dcf9837c64d69e091f7
Jan Honsbrok <jan.honsbrok@gmail.com>
2025-08-05 20:48:02 +0200

    tests: implement integration tests for png image format writer

---
+++ src/crayg/integrationTests/ImageAndFilmFixture.h
@@ -0,0 +1,90 @@
+#pragma once
+#include "image/Image.h"
+#include "image/film/Film.h"
+#include "image/imageiterators/pixels/ImageIterators.h"
+
+namespace crayg {
+
+struct ImageFixture {
+    ImageFixture(bool addMetadata = true) : image(Image(128, 72)) {
+        createRgbGradientImage(image.rgb);
+        if (addMetadata) {
+            addTestMetadata();
+        }
+    }
+
+    void addCustomChannel(PixelFormat pixelFormat, int colorChannelCount) {
+        auto buffer = std::make_unique<PixelBuffer>(128, 72, pixelFormat, colorChannelCount);
+        image.addChannel("custom", std::move(buffer));
+        createRgbGradientImage(*image.getChannel("custom"));
+    }
+
+    Image image;
+
+  private:
+    void createRgbGradientImage(PixelBuffer &pixelBuffer) {
+        for (auto pixel : ImageIterators::lineByLine(pixelBuffer)) {
+            const float relativeX = static_cast<float>(pixel.x) / pixelBuffer.getWidth();
+            const float relativeY = static_cast<float>(pixel.y) / pixelBuffer.getHeight();
+            if (relativeY <= 0.33) {
+                pixelBuffer.setValue(pixel, Color(relativeX, 0, 0));
+            } else if (relativeY <= 0.66) {
+                pixelBuffer.setValue(pixel, Color(0, relativeX, 0));
+            } else {
+                pixelBuffer.setValue(pixel, Color(0, 0, relativeX));
+            }
+        }
+    }
+
+    void addTestMetadata() {
+        image.metadata.write("test/int", 42);
+        image.metadata.write("test/float", 42.42f);
+        image.metadata.write("test/string", "teststring");
+        image.metadata.write("test/seconds", std::chrono::seconds(42));
+    }
+};
+
+struct FilmFixture {
+    FilmFixture(bool addMetadata = true) : film(Film(128, 72)) {
+        createRgbGradientImage("color");
+        if (addMetadata) {
+            addTestMetadata();
+        }
+    }
+
+    void addCustomChannel(PixelFormat pixelFormat, int channelCount) {
+        const FilmBufferVariantPtr &ptr =
+            FilmBufferFactory::createFilmBuffer({128, 72}, FilmBufferType::VALUE, pixelFormat, channelCount);
+        film.addChannel("custom", ptr);
+        createRgbGradientImage("custom");
+    }
+
+    Film film;
+
+  private:
+    void createRgbGradientImage(const std::string &channelName) {
+        const Resolution resolution = film.getResolution();
+        for (auto pixel : ImageIterators::lineByLine(resolution)) {
+            const float relativeX = static_cast<float>(pixel.x) / resolution.getWidth();
+            const float relativeY = static_cast<float>(pixel.y) / resolution.getHeight();
+
+            if (relativeY <= 0.33) {
+                film.addSample(channelName, pixel, Color(relativeX, 0, 0));
+            } else if (relativeY <= 0.66) {
+                film.addSample(channelName, pixel, Color(0, relativeX, 0));
+            } else {
+                film.addSample(channelName, pixel, Color(0, 0, relativeX));
+            }
+        }
+        film.updateAverages();
+    }
+
+    void addTestMetadata() {
+        film.metadata.write("test/int", 42);
+        film.metadata.write("test/float", 42.42f);
+        film.metadata.write("test/string", "teststring");
+        film.metadata.write("test/seconds", std::chrono::seconds(42));
+    }
+};
+
+}
---
+++ src/crayg/integrationTests/pngImageFormatWriter.cpp
@@ -0,0 +1,225 @@
+#include "ImageAndFilmFixture.h"
+#include "compatibility/lodepng/fmt/LodePNG_formatter.h"
+#include "image/imageiterators/pixels/ImageIterators.h"
+#include "image/io/imageformatwriters/ImageFormatWriters.h"
+#include "utils.h"
+#include <catch2/catch.hpp>
+#include <lodepng.h>
+#include <map>
+
+namespace crayg {
+
+struct PngAsserter {
+    PngAsserter(const std::filesystem::path &path) {
+        if (!std::filesystem::exists(path)) {
+            CRAYG_LOG_AND_THROW_RUNTIME_ERROR("File {} does not exist", path);
+        }
+        std::vector<unsigned char> png;
+        std::vector<unsigned char> image;
+        unsigned width, height;
+
+        unsigned error = lodepng::load_file(png, reinterpret_cast<const char *>(path.u8string().c_str()));
+        if (error) {
+            CRAYG_LOG_AND_THROW_RUNTIME_ERROR("Error when encoding PNG image at path {}: {}", path,
+                                              lodepng_error_text(error));
+        }
+
+        error = lodepng::decode(image, width, height, state, png);
+        if (error) {
+            CRAYG_LOG_AND_THROW_RUNTIME_ERROR("Error when encoding PNG image at path {}: {}", path,
+                                              lodepng_error_text(error));
+        }
+
+        for (int i = 0; i < state.info_png.text_num; i++) {
+            text[state.info_png.text_keys[i]] = state.info_png.text_strings[i];
+        }
+    }
+
+    LodePNGColorType getColorType() {
+        return state.info_png.color.colortype;
+    }
+
+    std::string getText(const std::string &key) {
+        if (text.find(key) == text.end()) {
+            CRAYG_LOG_AND_THROW_RUNTIME_ERROR("PNG file has no text for key '{}'", key);
+        }
+        return text[key];
+    }
+
+    bool hasNoText() const {
+        return text.empty();
+    }
+
+    void verifyPngHasExpectedTestMetadata() {
+        REQUIRE(getText("test/int") == "42");
+        REQUIRE(getText("test/float") == "42.42");
+        REQUIRE(getText("test/string") == "teststring");
+        REQUIRE(getText("test/seconds") == "42s");
+    }
+
+    lodepng::State state;
+    std::map<std::string, std::string> text;
+};
+
+void verifyPng(const std::filesystem::path &imagePath, LodePNGColorType expectedColorType,
+               bool hasExpectedTestMetadata = true) {
+    PngAsserter pngAsserter(imagePath);
+    REQUIRE(pngAsserter.getColorType() == expectedColorType);
+    if (hasExpectedTestMetadata) {
+        pngAsserter.verifyPngHasExpectedTestMetadata();
+    } else {
+        REQUIRE(pngAsserter.hasNoText());
+    }
+}
+
+std::string getImageFormatPngOutput(const std::string &testName) {
+    return getOutputFilename("ImageFormatWriters", testName, ".png");
+}
+
+std::string getImageFormatPngOutput(const std::string &testName, const std::string &channelName) {
+    return getOutputFilename("ImageFormatWriters", testName, fmt::format(".{}.png", channelName));
+}
+
+TEST_CASE("ImageFormatWriters/write_image_png_rgb_float") {
+    ImageFixture imageFixture;
+    const std::filesystem::path imagePath = getImageFormatPngOutput("write_image_png_rgb_float");
+
+    ImageFormatWriters::write(imagePath, imageFixture.image);
+
+    verifyPng(imagePath, LodePNGColorType::LCT_RGB);
+}
+
+TEST_CASE("ImageFormatWriters/write_image_png_customChannel_rgb_float") {
+    ImageFixture imageFixture;
+    imageFixture.addCustomChannel(PixelFormat::FLOAT32, 3);
+    const std::filesystem::path imagePath = getImageFormatPngOutput("write_image_png_customChannel_rgb_float");
+
+    ImageFormatWriters::write(imagePath, imageFixture.image);
+
+    verifyPng(imagePath, LodePNGColorType::LCT_RGB);
+    const std::filesystem::path customChannelPath =
+        getImageFormatPngOutput("write_image_png_customChannel_rgb_float", "custom");
+    verifyPng(customChannelPath, LodePNGColorType::LCT_RGB);
+}
+
+TEST_CASE("ImageFormatWriters/write_image_png_customChannel_rgb_int") {
+    ImageFixture imageFixture;
+    imageFixture.addCustomChannel(PixelFormat::UINT8, 3);
+    const std::filesystem::path imagePath = getImageFormatPngOutput("write_image_png_customChannel_rgb_int");
+
+    ImageFormatWriters::write(imagePath, imageFixture.image);
+
+    verifyPng(imagePath, LodePNGColorType::LCT_RGB);
+    const std::filesystem::path customChannelPath =
+        getImageFormatPngOutput("write_image_png_customChannel_rgb_int", "custom");
+    verifyPng(customChannelPath, LodePNGColorType::LCT_RGB);
+}
+
+TEST_CASE("ImageFormatWriters/write_image_png_customChannel_grey_float") {
+    ImageFixture imageFixture;
+    imageFixture.addCustomChannel(PixelFormat::FLOAT32, 1);
+    const std::filesystem::path imagePath = getImageFormatPngOutput("write_image_png_customChannel_grey_float");
+
+    ImageFormatWriters::write(imagePath, imageFixture.image);
+
+    verifyPng(imagePath, LodePNGColorType::LCT_RGB);
+    const std::filesystem::path customChannelPath =
+        getImageFormatPngOutput("write_image_png_customChannel_grey_float", "custom");
+    verifyPng(customChannelPath, LodePNGColorType::LCT_GREY);
+}
+
+TEST_CASE("ImageFormatWriters/write_image_png_customChannel_grey_int") {
+    ImageFixture imageFixture;
+    imageFixture.addCustomChannel(PixelFormat::UINT8, 1);
+    const std::filesystem::path imagePath = getImageFormatPngOutput("write_image_png_customChannel_grey_int");
+
+    ImageFormatWriters::write(imagePath, imageFixture.image);
+
+    verifyPng(imagePath, LodePNGColorType::LCT_RGB);
+    const std::filesystem::path customChannelPath =
+        getImageFormatPngOutput("write_image_png_customChannel_grey_int", "custom");
+    verifyPng(customChannelPath, LodePNGColorType::LCT_GREY);
+}
+
+TEST_CASE("ImageFormatWriters/write_image_empty_metadata") {
+    ImageFixture imageFixture(false);
+    const std::filesystem::path imagePath =
+        getOutputFilename("ImageFormatWriters", "write_image_empty_metadata", ".png");
+
+    ImageFormatWriters::write(imagePath, imageFixture.image);
+
+    verifyPng(imagePath, LodePNGColorType::LCT_RGB, false);
+}
+
+TEST_CASE("ImageFormatWriters/write_film_png_rgb_float") {
+    FilmFixture filmFixture;
+    const std::filesystem::path imagePath = getImageFormatPngOutput("write_film_png_rgb_float");
+
+    ImageFormatWriters::write(imagePath, filmFixture.film);
+
+    verifyPng(imagePath, LodePNGColorType::LCT_RGB);
+}
+
+TEST_CASE("ImageFormatWriters/write_film_png_customChannel_rgb_float") {
+    FilmFixture filmFixture;
+    filmFixture.addCustomChannel(PixelFormat::FLOAT32, 3);
+    const std::filesystem::path imagePath = getImageFormatPngOutput("write_film_png_customChannel_rgb_float");
+
+    ImageFormatWriters::write(imagePath, filmFixture.film);
+
+    verifyPng(imagePath, LodePNGColorType::LCT_RGB);
+    const std::filesystem::path customChannelPath =
+        getImageFormatPngOutput("write_film_png_customChannel_rgb_float", "custom");
+    verifyPng(customChannelPath, LodePNGColorType::LCT_RGB);
+}
+
+TEST_CASE("ImageFormatWriters/write_film_png_customChannel_rgb_int") {
+    FilmFixture filmFixture;
+    filmFixture.addCustomChannel(PixelFormat::UINT8, 3);
+    const std::filesystem::path imagePath = getImageFormatPngOutput("write_film_png_customChannel_rgb_int");
+
+    ImageFormatWriters::write(imagePath, filmFixture.film);
+
+    verifyPng(imagePath, LodePNGColorType::LCT_RGB);
+    const std::filesystem::path customChannelPath =
+        getImageFormatPngOutput("write_film_png_customChannel_rgb_int", "custom");
+    verifyPng(customChannelPath, LodePNGColorType::LCT_RGB);
+}
+
+TEST_CASE("ImageFormatWriters/write_film_png_customChannel_grey_float") {
+    FilmFixture filmFixture;
+    filmFixture.addCustomChannel(PixelFormat::FLOAT32, 1);
+    const std::filesystem::path imagePath = getImageFormatPngOutput("write_film_png_customChannel_grey_float");
+
+    ImageFormatWriters::write(imagePath, filmFixture.film);
+
+    verifyPng(imagePath, LodePNGColorType::LCT_RGB);
+    const std::filesystem::path customChannelPath =
+        getImageFormatPngOutput("write_film_png_customChannel_grey_float", "custom");
+    verifyPng(customChannelPath, LodePNGColorType::LCT_GREY);
+}
+
+TEST_CASE("ImageFormatWriters/write_film_png_customChannel_grey_int") {
+    FilmFixture filmFixture;
+    filmFixture.addCustomChannel(PixelFormat::UINT8, 1);
+    const std::filesystem::path imagePath = getImageFormatPngOutput("write_film_png_customChannel_grey_int");
+
+    ImageFormatWriters::write(imagePath, filmFixture.film);
+
+    verifyPng(imagePath, LodePNGColorType::LCT_RGB);
+    const std::filesystem::path customChannelPath =
+        getImageFormatPngOutput("write_film_png_customChannel_grey_int", "custom");
+    verifyPng(customChannelPath, LodePNGColorType::LCT_GREY);
+}
+
+TEST_CASE("ImageFormatWriters/write_film_empty_metadata") {
+    FilmFixture filmFixture(false);
+    const std::filesystem::path imagePath =
+        getOutputFilename("ImageFormatWriters", "write_film_empty_metadata", ".png");
+
+    ImageFormatWriters::write(imagePath, filmFixture.film);
+
+    verifyPng(imagePath, LodePNGColorType::LCT_RGB, false);
+}
+
+}
