6b8257531d1dbe3b7830ee505b6234a2984af09c
Jan Honsbrok <jan.honsbrok@gmail.com>
2025-08-06 17:00:29 +0200

    fix: make sure formatter work in fmt and Catch2

--- src/crayg/src/compatibility/openexr/fmt/ImfCompression_formatter.h
+++ src/crayg/src/compatibility/openexr/fmt/ImfCompression_formatter.h
@@ -1,5 +1,14 @@
 #pragma once
-#include "utils/EnumFormatter.h"
 #include <ImfCompression.h>
+#include <fmt/ostream.h>
 
-CRAYG_FMT_ENUM_FORMATTER(Imf::Compression);
+namespace std {
+
+inline std::ostream &operator<<(std::ostream &os, Imf::Compression value) {
+    os << magic_enum::enum_name(value);
+    return os;
+}
+
+}
+
+template <> struct fmt::formatter<Imf::Compression> : ostream_formatter {};
--- src/crayg/src/compatibility/openexr/fmt/ImfPixelType_formatter.h
+++ src/crayg/src/compatibility/openexr/fmt/ImfPixelType_formatter.h
@@ -1,5 +1,14 @@
 #pragma once
-#include "utils/EnumFormatter.h"
 #include <ImfPixelType.h>
+#include <fmt/ostream.h>
 
-CRAYG_FMT_ENUM_FORMATTER(Imf::PixelType);
+namespace std {
+
+inline std::ostream &operator<<(std::ostream &os, Imf::PixelType value) {
+    os << magic_enum::enum_name(value);
+    return os;
+}
+
+}
+
+template <> struct fmt::formatter<Imf::PixelType> : ostream_formatter {};
