845c6afa2232da4f2e39893ab6bdfd3c01b23a3a
Jan Honsbrok <jan.honsbrok@gmail.com>
2025-08-20 09:37:32 +0200

    fix: remove unnecessary headers

--- src/crayg/src/compatibility/usd/fmt/SdfPath_formatter.h
+++ src/crayg/src/compatibility/usd/fmt/SdfPath_formatter.h
@@ -3,11 +3,6 @@
 #include <fmt/format.h>
 #include <pxr/usd/sdf/path.h>
 
-#include <fmt/format.h>
-#include <pxr/base/tf/token.h>
-#include <pxr/usd/sdf/valueTypeName.h>
-#include <pxr/usd/usd/stage.h>
-
 template <> struct fmt::formatter<pxr::SdfPath> {
     template <typename ParseContext> constexpr auto parse(ParseContext &ctx) {
         return ctx.begin();
