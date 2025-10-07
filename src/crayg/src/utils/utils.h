b3fbe7d233a6bc1d6a61006b353ebf281be90933
Jan Honsbrok <jan.honsbrok@gmail.com>
2025-10-07 22:05:03 +0200

    refactor: remove unused function

--- src/crayg/src/utils/utils.h
+++ src/crayg/src/utils/utils.h
@@ -5,10 +5,6 @@
 
 namespace crayg {
 
-inline bool compareWithPrecision(float A, float B, float epsilon = 0.001f) {
-    return (fabs(A - B) < epsilon);
-}
-
 std::string stripCommentFromLine(std::string line);
 
 }
