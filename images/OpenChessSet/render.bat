d6fa470c2f8e1a552d16a7cde7e5ec088d01cd4e
Jan Honsbrok <jan.honsbrok@gmail.com>
2025-10-07 16:55:32 +0200

    chore: add script to render some pretty images

---
+++ images/OpenChessSet/render.bat
@@ -0,0 +1,23 @@
+C:\workspace\crayg\cmake-build-relwithdebinfo\bin\crayg-standalone -s M:\Test_Scene_Collection\OpenChessSet\OpenChessSetCrayg.usda -o OpenChessSet.#.exr --camera /cams/render_cam_top_side_2/render_cam_top_side_2Shape --variantSelection /cams/render_cam_top_side_2:focus_distance=turm --resolution 1920x1080 --maxSamples 4096 --tileSamplerType=uniform
+
+C:\workspace\crayg\cmake-build-relwithdebinfo\bin\crayg-standalone -s M:\Test_Scene_Collection\OpenChessSet\OpenChessSetCrayg.usda -o OpenChessSet.#.exr --camera /cams/render_cam_top_side_2/render_cam_top_side_2Shape --variantSelection /cams/render_cam_top_side_2:focus_distance=dame --resolution 1920x1080 --maxSamples 4096 --tileSamplerType=uniform
+
+C:\workspace\crayg\cmake-build-relwithdebinfo\bin\crayg-standalone -s M:\Test_Scene_Collection\OpenChessSet\OpenChessSetCrayg.usda -o OpenChessSet.#.exr --camera /cams/render_cam_top_side_2/render_cam_top_side_2Shape --variantSelection /cams/render_cam_top_side_2:focus_distance=springer --resolution 1920x1080 --maxSamples 4096 --tileSamplerType=uniform
+
+C:\workspace\crayg\cmake-build-relwithdebinfo\bin\crayg-standalone -s M:\Test_Scene_Collection\OpenChessSet\OpenChessSetCrayg.usda -o OpenChessSet.#.exr --camera /cams/render_cam_top_side_2/render_cam_top_side_2Shape --variantSelection /cams/render_cam_top_side_2:focus_distance=koenig --resolution 1920x1080 --maxSamples 4096 --tileSamplerType=uniform
+
+C:\workspace\crayg\cmake-build-relwithdebinfo\bin\crayg-standalone -s M:\Test_Scene_Collection\OpenChessSet\OpenChessSetCrayg.usda -o OpenChessSet.#.exr --camera /cams/render_cam_top_side_2/render_cam_top_side_2_70mm --variantSelection /cams/render_cam_top_side_2:focus_distance=turm --resolution 1920x1080 --maxSamples 4096 --tileSamplerType=uniform --useSpectralLensing
+
+C:\workspace\crayg\cmake-build-relwithdebinfo\bin\crayg-standalone -s M:\Test_Scene_Collection\OpenChessSet\OpenChessSetCrayg.usda -o OpenChessSet.#.exr --camera /cams/render_cam_top_side_2/render_cam_top_side_2_70mm --variantSelection /cams/render_cam_top_side_2:focus_distance=dame --resolution 1920x1080 --maxSamples 4096 --tileSamplerType=uniform --useSpectralLensing
+
+C:\workspace\crayg\cmake-build-relwithdebinfo\bin\crayg-standalone -s M:\Test_Scene_Collection\OpenChessSet\OpenChessSetCrayg.usda -o OpenChessSet.#.exr --camera /cams/render_cam_top_side_2/render_cam_top_side_2_70mm --variantSelection /cams/render_cam_top_side_2:focus_distance=springer --resolution 1920x1080 --maxSamples 4096 --tileSamplerType=uniform --useSpectralLensing
+
+C:\workspace\crayg\cmake-build-relwithdebinfo\bin\crayg-standalone -s M:\Test_Scene_Collection\OpenChessSet\OpenChessSetCrayg.usda -o OpenChessSet.#.exr --camera /cams/render_cam_top_side_2/render_cam_top_side_2_70mm --variantSelection /cams/render_cam_top_side_2:focus_distance=koenig --resolution 1920x1080 --maxSamples 4096 --tileSamplerType=uniform --useSpectralLensing
+
+C:\workspace\crayg\cmake-build-relwithdebinfo\bin\crayg-standalone -s M:\Test_Scene_Collection\OpenChessSet\OpenChessSetCrayg.usda -o OpenChessSet.#.exr --camera /cams/render_cam_top/render_cam_topShape --resolution 1920x1080 --maxSamples 4096 --tileSamplerType=uniform
+
+C:\workspace\crayg\cmake-build-relwithdebinfo\bin\crayg-standalone -s M:\Test_Scene_Collection\OpenChessSet\OpenChessSetCrayg.usda -o OpenChessSet.#.exr --camera /cams/render_cam_top_70mm/render_cam_top_70mmShape --resolution 1920x1080 --maxSamples 4096 --tileSamplerType=uniform --useSpectralLensing
+
+C:\workspace\crayg\cmake-build-relwithdebinfo\bin\crayg-standalone -s M:\Test_Scene_Collection\OpenChessSet\OpenChessSetCrayg.usda -o OpenChessSet.#.exr --camera /cams/render_cam_detail/render_cam_detailShape --resolution 1920x1080 --maxSamples 4096 --tileSamplerType=uniform
+
+C:\workspace\crayg\cmake-build-relwithdebinfo\bin\crayg-standalone -s M:\Test_Scene_Collection\OpenChessSet\OpenChessSetCrayg.usda -o OpenChessSet.#.exr --camera /cams/render_cam_top_side/render_cam_top_sideShape --resolution 1920x1080 --maxSamples 4096 --tileSamplerType=uniform
