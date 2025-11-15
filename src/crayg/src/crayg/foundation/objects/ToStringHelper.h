cd4055eda2836bfef3343d4464766c450b05d156
Jan Honsbrok <jan.honsbrok@gmail.com>
2025-11-15 15:53:30 +0100

    feat: introduce improved version of ToStringHelper

--- src/crayg/src/crayg/foundation/objects/ToStringHelper.h
+++ src/crayg/src/crayg/foundation/objects/ToStringHelper.h
@@ -36,4 +36,31 @@ class ToStringHelper {
     bool hasMembers = false;
 };
 
+class ToStringHelper2 {
+  public:
+    explicit ToStringHelper2(std::ostream &os, const std::string_view &className) : os(os) {
+        os << className << '{';
+    }
+
+    template <typename T> ToStringHelper2 &addMember(const std::string_view &name, const T &value) {
+        if (hasMembers) {
+            os << ',';
+        }
+
+        fmt::print(os, "{}={}", name, value);
+
+        hasMembers = true;
+        return *this;
+    };
+
+    std::ostream &finish() const {
+        os << '}';
+        return os;
+    }
+
+  private:
+    std::ostream &os;
+    bool hasMembers = false;
+};
+
 }
--- src/crayg/tests/crayg/foundation/objects/TestToStringHelper.cpp
+++ src/crayg/tests/crayg/foundation/objects/TestToStringHelper.cpp
@@ -1,5 +1,8 @@
 #include "crayg/foundation/objects/ToStringHelper.h"
+#include "crayg/foundation/objects/ToStringHelperMacros.h"
+
 #include <catch2/catch.hpp>
+#include <ostream>
 
 namespace crayg {
 
@@ -19,6 +22,59 @@ TEST_CASE("ToStringHelper", "[ToStringHelper]") {
         helper.addMember("myMember", 1).addMember("myMember2", 2);
         REQUIRE(helper.finish() == "TestClass{myMember=1,myMember2=2}");
     }
+}
+
+TEST_CASE("ToStringHelper2", "[ToStringHelper]") {
+    std::ostringstream os;
+    ToStringHelper2 helper(os, "TestClass");
+
+    SECTION("emptyClass") {
+        helper.finish();
+        REQUIRE(os.str() == "TestClass{}");
+    }
+
+    SECTION("oneMember") {
+        helper.addMember("myMember", 1).finish();
+        REQUIRE(os.str() == "TestClass{myMember=1}");
+    }
+
+    SECTION("twoMembers") {
+        helper.addMember("myMember", 1).addMember("myMember2", 2).finish();
+        REQUIRE(os.str() == "TestClass{myMember=1,myMember2=2}");
+    }
+}
+
+namespace tostringhelpertests {
+
+struct Example {
+    std::string name;
+    int a;
+
+    friend std::ostream &operator<<(std::ostream &os, const Example &obj);
+};
+
+CRAYG_TO_STRING_HELPER_OSTREAM_IMPL(Example, name, a);
+
+TEST_CASE("ToStringHelper macros", "[ToStringHelper]") {
+    Example obj{"test", 42};
+
+    SECTION("should generate correct ToStringHelper") {
+        std::ostringstream os;
+
+        CRAYG_TO_STRING_HELPER(os, Example, name, a);
+
+        REQUIRE(os.str() == "Example{name=test,a=42}");
+    }
+
+    SECTION("should generate correct implementation of ostream operator") {
+        std::ostringstream os;
+
+        os << obj;
+
+        REQUIRE(os.str() == "Example{name=test,a=42}");
+    }
+}
+
 }
 
 }
